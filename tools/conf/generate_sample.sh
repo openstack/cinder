0094303e36cf1c01db539a1eea7771ebcb4d336a
Dan Prince <dprince@redhat.com>
2012-09-05 12:59:28 -0400

    Sync improvements to config file generator tools.
    We made some improvements to the config file generator in
    Nova during Folsom including spacing and formatting changes.
    
    This patch merges these into Cinder so we can make use of
    them to generate a new sample config file.
    
    Change-Id: I1e1166ac6e2a28cc179223e5af547995f58b36e6

--- tools/conf/generate_sample.sh
+++ tools/conf/generate_sample.sh
@@ -16,10 +16,9 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-pushd $(cd $(dirname "$0") && pwd) >/dev/null
+FILES=$(find cinder -type f -name "*.py" ! -path "cinder/tests/*" -exec \
+    grep -l "Opt(" {} \; | sort -u)
 
-find ../../cinder -type f -name "*.py" ! -path "../../cinder/tests/*" -exec \
-    grep -l "Opt(" {} \; | sort -u | xargs python create_conf.py > \
-    ../../etc/cinder/cinder.conf.sample
-
-popd >/dev/null
+PYTHONPATH=./:${PYTHONPATH} \
+    python $(dirname "$0")/extract_opts.py ${FILES} > \
+    etc/cinder/cinder.conf.sample
