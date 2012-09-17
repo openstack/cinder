9696bd9a8c11618edf970812de822ad0ba99498c
Zhiteng Huang <zhiteng.huang@intel.com>
2012-09-17 17:26:09 +0800

    Update quota when deleting volume that failed to be scheduled
    If one volume was failed to get scheduled, removing such volume should
    also clean up reservation.
    Also when create_volume is ready to send to scheduler, reservation
    should be committed no matter backend can successfully create that
    volume or not since deleting volume call will do a minus reservation
    even on volume with 'error' status.
    
    Fix bug 1052052
    
    Change-Id: Iaec9926024ad5adb38b08b07772095f49b8593c2

--- cinder/volume/api.py
+++ cinder/volume/api.py
@@ -164,12 +164,15 @@ class API(base.Base):
             }
 
         volume = self.db.volume_create(context, options)
+
+        QUOTAS.commit(context, reservations)
+
         self._cast_create_volume(context, volume['id'], snapshot_id,
-                                 image_id, reservations)
+                                 image_id)
         return volume
 
     def _cast_create_volume(self, context, volume_id, snapshot_id,
-                            image_id, reservations):
+                            image_id):
 
         # NOTE(Rongze Zhu): It is a simple solution for bug 1008866
         # If snapshot_id is set, make the call create volume directly to
@@ -188,8 +191,7 @@ class API(base.Base):
                      {"method": "create_volume",
                       "args": {"volume_id": volume_id,
                                "snapshot_id": snapshot_id,
-                               "image_id": image_id,
-                               "reservations": reservations}})
+                               "image_id": image_id}})
         else:
             rpc.cast(context,
                      FLAGS.scheduler_topic,
@@ -197,15 +199,25 @@ class API(base.Base):
                       "args": {"topic": FLAGS.volume_topic,
                                "volume_id": volume_id,
                                "snapshot_id": snapshot_id,
-                               "image_id": image_id,
-                               "reservations": reservations}})
+                               "image_id": image_id}})
 
     @wrap_check_policy
     def delete(self, context, volume, force=False):
         volume_id = volume['id']
         if not volume['host']:
             # NOTE(vish): scheduling failed, so delete it
+            # Note(zhiteng): update volume quota reservation
+            try:
+                reservations = QUOTAS.reserve(context, volumes=-1,
+                                              gigabytes=-volume['size'])
+            except Exception:
+                reservations = None
+                LOG.exception(_("Failed to update quota for deleting volume"))
+
             self.db.volume_destroy(context, volume_id)
+
+            if reservations:
+                QUOTAS.commit(context, reservations)
             return
         if not force and volume['status'] not in ["available", "error"]:
             msg = _("Volume status must be available or error")
