826c4520d093f5e0809a08e10b5d18bf69adc698
Brian Waldon <bcwaldon@gmail.com>
2012-10-27 15:36:30 -0700

    Add VolumeHostAttribute API extension
    Expose the host on which a volume resides through a new API
    extension. This is only presented to Admins by default. This
    can be controlled with the 'volume_host_attribute' policy
    rule.
    
    Fixes bug 1035350.
    
    Change-Id: I0a74a0dfbd78e853219150fbe0d3fba77c6f9bb6

---
+++ cinder/api/openstack/volume/contrib/volume_host_attribute.py
@@ -0,0 +1,93 @@
+#   Copyright 2012 OpenStack, LLC.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+from cinder.api.openstack import extensions
+from cinder.api.openstack import wsgi
+from cinder.api.openstack import xmlutil
+from cinder.openstack.common import log as logging
+from cinder import volume
+
+
+LOG = logging.getLogger(__name__)
+authorize = extensions.soft_extension_authorizer('volume',
+                                                 'volume_host_attribute')
+
+
+class VolumeHostAttributeController(wsgi.Controller):
+    def __init__(self, *args, **kwargs):
+        super(VolumeHostAttributeController, self).__init__(*args, **kwargs)
+        self.volume_api = volume.API()
+
+    def _add_volume_host_attribute(self, context, resp_volume):
+        try:
+            db_volume = self.volume_api.get(context, resp_volume['id'])
+        except Exception:
+            return
+        else:
+            key = "%s:host" % Volume_host_attribute.alias
+            resp_volume[key] = db_volume['host']
+
+    @wsgi.extends
+    def show(self, req, resp_obj, id):
+        context = req.environ['cinder.context']
+        if authorize(context):
+            resp_obj.attach(xml=VolumeHostAttributeTemplate())
+            self._add_volume_host_attribute(context, resp_obj.obj['volume'])
+
+    @wsgi.extends
+    def detail(self, req, resp_obj):
+        context = req.environ['cinder.context']
+        if authorize(context):
+            resp_obj.attach(xml=VolumeListHostAttributeTemplate())
+            for volume in list(resp_obj.obj['volumes']):
+                self._add_volume_host_attribute(context, volume)
+
+
+class Volume_host_attribute(extensions.ExtensionDescriptor):
+    """Expose host as an attribute of a volume."""
+
+    name = "VolumeHostAttribute"
+    alias = "os-vol-host-attr"
+    namespace = ("http://docs.openstack.org/volume/ext/"
+                 "volume_host_attribute/api/v1")
+    updated = "2011-11-03T00:00:00+00:00"
+
+    def get_controller_extensions(self):
+        controller = VolumeHostAttributeController()
+        extension = extensions.ControllerExtension(self, 'volumes', controller)
+        return [extension]
+
+
+def make_volume(elem):
+    elem.set('{%s}host' % Volume_host_attribute.namespace,
+             '%s:host' % Volume_host_attribute.alias)
+
+
+class VolumeHostAttributeTemplate(xmlutil.TemplateBuilder):
+    def construct(self):
+        root = xmlutil.TemplateElement('volume', selector='volume')
+        make_volume(root)
+        alias = Volume_host_attribute.alias
+        namespace = Volume_host_attribute.namespace
+        return xmlutil.SlaveTemplate(root, 1, nsmap={alias: namespace})
+
+
+class VolumeListHostAttributeTemplate(xmlutil.TemplateBuilder):
+    def construct(self):
+        root = xmlutil.TemplateElement('volumes')
+        elem = xmlutil.SubTemplateElement(root, 'volume', selector='volumes')
+        make_volume(elem)
+        alias = Volume_host_attribute.alias
+        namespace = Volume_host_attribute.namespace
+        return xmlutil.SlaveTemplate(root, 1, nsmap={alias: namespace})
