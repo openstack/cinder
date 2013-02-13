275ce2d171d847ede99162a3dbf85d87999a7298
Mate Lakat <mate.lakat@citrix.com>
2013-02-13 10:18:15 +0000

    Fix PEP8 violation
    There was a PEP8 violation in cinder/tests/test_solidfire.py, thus
    ./run_tests.sh was failing. This patch fixes the error.
    
    Change-Id: I444fbfa57550db5c75b80683054c0ca44f4ee657

--- cinder/tests/test_solidfire.py
+++ cinder/tests/test_solidfire.py
@@ -76,7 +76,7 @@ class SolidFireVolumeTestCase(test.TestCase):
                              'enable512e': True,
                              'access': "readWrite",
                              'status': "active",
-                             'attributes':None,
+                             'attributes': None,
                              'qos': None,
                              'iqn': test_name}]}}
             return result
