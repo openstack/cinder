00e8049cbec550a0342054a02fc3ea35be25dad0
Rushi Agrawal <rushi.agr@gmail.com>
2013-03-12 01:39:44 +0530

    NetApp[backport]: Fix for snapshot not deleted in error state.
    This fix enables cleaning of volumes/snapshots
    in error state by allowing them to delete.
    It will allow the delete call to be
    successful if snapshot/volume are in
    error state. This also fixes the bug
    raised recently with  number 1145409 which seems
    to be duplicate of 1143661.
    
    bug 1143661
    bug 1145409
    
    Change-Id: I38bc13e676404f96db54b12edfa56680489e893e

--- cinder/volume/netapp.py
+++ cinder/volume/netapp.py
@@ -1129,6 +1129,10 @@ class NetAppCmodeISCSIDriver(driver.ISCSIDriver):
         """Driver entry point for destroying existing volumes."""
         name = volume['name']
         handle = self._get_lun_handle(name)
+        if not handle:
+            msg = _("No entry in LUN table for volume %(name)s.")
+            LOG.warn(msg % locals())
+            return
         self.client.service.DestroyLun(Handle=handle)
         LOG.debug(_("Destroyed LUN %s") % handle)
         self.lun_table.pop(name)
@@ -1236,7 +1240,12 @@ class NetAppCmodeISCSIDriver(driver.ISCSIDriver):
 
     def delete_snapshot(self, snapshot):
         """Driver entry point for deleting a snapshot."""
-        handle = self._get_lun_handle(snapshot['name'])
+        name = snapshot['name']
+        handle = self._get_lun_handle(name)
+        if not handle:
+            msg = _("No entry in LUN table for snapshot %s.")
+            LOG.warn(msg % name)
+            return
         self.client.service.DestroyLun(Handle=handle)
         LOG.debug(_("Destroyed LUN %s") % handle)
 
