d0b9b2721045454bcb076537af97be9b14a1103d
Walter A. Boring IV <walter.boring@hp.com>
2013-03-20 13:18:12 -0700

    Update Cinder's latest copy of OSLO grizzly stable
    This patch updates all the files from oslo under
    stable/grizzly but policy.py.  policy.py is a high risk
    change at this date and it has lots of changes since the
    last time we updated it.
    
    Fixes bug #1157126
    
    Change-Id: I399a1cd8474f718ed5196def90d2fea546fb01f5
    (cherry picked from commit eae0264782818342c222c18b549e9cda354c867b)

--- cinder/openstack/common/local.py
+++ cinder/openstack/common/local.py
@@ -1,6 +1,6 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-# Copyright 2011 OpenStack LLC.
+# Copyright 2011 OpenStack Foundation.
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -26,6 +26,9 @@ class WeakLocal(corolocal.local):
     def __getattribute__(self, attr):
         rval = corolocal.local.__getattribute__(self, attr)
         if rval:
+            # NOTE(mikal): this bit is confusing. What is stored is a weak
+            # reference, not the value itself. We therefore need to lookup
+            # the weak reference and return the inner value here.
             rval = rval()
         return rval
 
@@ -34,4 +37,12 @@ class WeakLocal(corolocal.local):
         return corolocal.local.__setattr__(self, attr, value)
 
 
+# NOTE(mikal): the name "store" should be deprecated in the future
 store = WeakLocal()
+
+# A "weak" store uses weak references and allows an object to fall out of scope
+# when it falls out of scope in the code that uses the thread local storage. A
+# "strong" store will hold a reference to the object so that it never falls out
+# of scope.
+weak_store = WeakLocal()
+strong_store = corolocal.local
