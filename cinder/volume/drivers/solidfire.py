6022e14e9f8700c1e1ec0df9dbb4a04cc1097309
john-griffith <john.griffith@solidfire.com>
2013-03-22 18:01:02 -0600

    Last driver sync for Folsom and Grizzly
    This is the last of the updates that I've been putting
    off while trying to handle the core items in Cinder.
    
    This change fixes the ctxt ordering error in volume_types
    and also fixes and also changes the internal SolidFire
    metadata storage to a single dict instead of a dict of dicts.
    
    Change-Id: I002a591bc109ae1ab625c89f6ad841f60e464a40
    (cherry picked from commit f53d48ebd9ff0e3b99470a9b95527c7054709c12)

--- cinder/volume/drivers/solidfire.py
+++ cinder/volume/drivers/solidfire.py
@@ -312,7 +312,8 @@ class SolidFire(SanISCSIDriver):
                       'src_uuid': 'src_uuid'}
 
         if qos:
-            attributes['qos'] = qos
+            for k, v in qos.items():
+                            attributes[k] = str(v)
 
         params = {'volumeID': int(sf_vol['volumeID']),
                   'name': 'UUID-%s' % v_ref['id'],
@@ -363,7 +364,7 @@ class SolidFire(SanISCSIDriver):
                     qos[i.key] = int(i.value)
         return qos
 
-    def _set_qos_by_volume_type(self, type_id, ctxt):
+    def _set_qos_by_volume_type(self, ctxt, type_id):
         qos = {}
         volume_type = volume_types.get_volume_type(ctxt, type_id)
         specs = volume_type.get('extra_specs')
@@ -432,7 +433,8 @@ class SolidFire(SanISCSIDriver):
         attributes = {'uuid': volume['id'],
                       'is_clone': 'False'}
         if qos:
-            attributes['qos'] = qos
+            for k, v in qos.items():
+                            attributes[k] = str(v)
 
         params = {'name': 'UUID-%s' % volume['id'],
                   'accountID': None,
