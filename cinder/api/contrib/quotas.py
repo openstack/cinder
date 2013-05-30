81f7f0475b8907222aaec464b7c0e56f8e331fb1
Nikolay Sobolevskiy <nsobolevsky@mirantis.com>
2013-05-30 23:21:09 +0400

    Fix incorrect authorization rule in quota contrib api.
    Incorrect policy and authorization extension name in
     contrib quota api. Legacy from nova.
    
    Change-Id: Ida8262ad63d6f881dd50a385a9913c2ed57b441c
    Fixes: bug #1185974
    (cherry picked from commit f7d4210d7c6c4c2f6f78f24d1681a110f49550d4)

--- cinder/api/contrib/quotas.py
+++ cinder/api/contrib/quotas.py
@@ -29,8 +29,8 @@ from cinder import quota
 QUOTAS = quota.QUOTAS
 
 
-authorize_update = extensions.extension_authorizer('compute', 'quotas:update')
-authorize_show = extensions.extension_authorizer('compute', 'quotas:show')
+authorize_update = extensions.extension_authorizer('volume', 'quotas:update')
+authorize_show = extensions.extension_authorizer('volume', 'quotas:show')
 
 
 class QuotaTemplate(xmlutil.TemplateBuilder):
@@ -111,7 +111,7 @@ class Quotas(extensions.ExtensionDescriptor):
 
     name = "Quotas"
     alias = "os-quota-sets"
-    namespace = "http://docs.openstack.org/compute/ext/quotas-sets/api/v1.1"
+    namespace = "http://docs.openstack.org/volume/ext/quotas-sets/api/v1.1"
     updated = "2011-08-08T00:00:00+00:00"
 
     def get_resources(self):
