e25c105740bb8a10ad05b188f8262af940539baa
Lakhinder Walia <lakhinder.walia@hds.com>
2013-07-11 18:27:16 -0700

    Minor reorg for (array resource usage and backend options naming)
    1. Use HUS target/initiator resources efficiently.
    2. Command options aligned with openStack scheme of using dashes.
    3. Self tests modified to accomodate #1 and #2.
    
    Change-Id: I7d23a980f237c973538ca08215f77a1f69cc2517
    Fixes: bug 1200441

--- cinder/volume/drivers/hds/hus_backend.py
+++ cinder/volume/drivers/hds/hus_backend.py
@@ -27,8 +27,9 @@ LOG = logging.getLogger("cinder.volume.driver")
 
 class HusBackend:
     """Back end. Talks to HUS."""
-    def get_version(self, cmd, ip0, ip1, user, pw):
+    def get_version(self, cmd, ver, ip0, ip1, user, pw):
         out, err = utils.execute(cmd,
+                                 '--driver-version', ver,
                                  '--ip0', ip0,
                                  '--ip1', ip1,
                                  '--user', user,
@@ -39,8 +40,9 @@ class HusBackend:
         LOG.debug('get_version: ' + out + ' -- ' + err)
         return out
 
-    def get_iscsi_info(self, cmd, ip0, ip1, user, pw):
+    def get_iscsi_info(self, cmd, ver, ip0, ip1, user, pw):
         out, err = utils.execute(cmd,
+                                 '--driver-version', ver,
                                  '--ip0', ip0,
                                  '--ip1', ip1,
                                  '--user', user,
@@ -50,8 +52,9 @@ class HusBackend:
         LOG.debug('get_iscsi_info: ' + out + ' -- ' + err)
         return out
 
-    def get_hdp_info(self, cmd, ip0, ip1, user, pw):
+    def get_hdp_info(self, cmd, ver, ip0, ip1, user, pw):
         out, err = utils.execute(cmd,
+                                 '--driver-version', ver,
                                  '--ip0', ip0,
                                  '--ip1', ip1,
                                  '--user', user,
@@ -61,14 +64,16 @@ class HusBackend:
         LOG.debug('get_hdp_info: ' + out + ' -- ' + err)
         return out
 
-    def create_lu(self, cmd, ip0, ip1, user, pw, id, hdp, start, end, size):
+    def create_lu(self, cmd, ver, ip0, ip1, user, pw, id, hdp, start,
+                  end, size):
         out, err = utils.execute(cmd,
+                                 '--driver-version', ver,
                                  '--ip0', ip0,
                                  '--ip1', ip1,
                                  '--user', user,
                                  '--password', pw,
-                                 '--create_lun', '1',
-                                 '--array_id', id,
+                                 '--create-lun', '1',
+                                 '--array-id', id,
                                  '--hdp', hdp,
                                  '--start', start,
                                  '--end', end,
@@ -77,28 +82,30 @@ class HusBackend:
         LOG.debug('create_lu: ' + out + ' -- ' + err)
         return out
 
-    def delete_lu(self, cmd, ip0, ip1, user, pw, id, lun):
+    def delete_lu(self, cmd, ver, ip0, ip1, user, pw, id, lun):
         out, err = utils.execute(cmd,
+                                 '--driver-version', ver,
                                  '--ip0', ip0,
                                  '--ip1', ip1,
                                  '--user', user,
                                  '--password', pw,
-                                 '--delete_lun', '1',
-                                 '--array_id', id,
+                                 '--delete-lun', '1',
+                                 '--array-id', id,
                                  '--lun', lun,
                                  check_exit_code=True)
         LOG.debug('delete_lu: ' + out + ' -- ' + err)
         return out
 
-    def create_dup(self, cmd, ip0, ip1, user, pw, id, src_lun,
+    def create_dup(self, cmd, ver, ip0, ip1, user, pw, id, src_lun,
                    hdp, start, end, size):
         out, err = utils.execute(cmd,
+                                 '--driver-version', ver,
                                  '--ip0', ip0,
                                  '--ip1', ip1,
                                  '--user', user,
                                  '--password', pw,
-                                 '--create_dup', '1',
-                                 '--array_id', id,
+                                 '--create-dup', '1',
+                                 '--array-id', id,
                                  '--pvol', src_lun,
                                  '--hdp', hdp,
                                  '--start', start,
@@ -108,35 +115,35 @@ class HusBackend:
         LOG.debug('create_dup: ' + out + ' -- ' + err)
         return out
 
-    def add_iscsi_conn(self, cmd, ip0, ip1, user, pw, id, lun, ctl, port, iqn,
-                       tgt_alias, initiator, init_alias):
+    def add_iscsi_conn(self, cmd, ver, ip0, ip1, user, pw, id, lun, ctl, port,
+                       iqn, initiator):
         out, err = utils.execute(cmd,
+                                 '--driver-version', ver,
                                  '--ip0', ip0,
                                  '--ip1', ip1,
                                  '--user', user,
                                  '--password', pw,
-                                 '--add_iscsi_connection', '1',
-                                 '--array_id', id,
+                                 '--add-iscsi-connection', '1',
+                                 '--array-id', id,
                                  '--lun', lun,
                                  '--ctl', ctl,
                                  '--port', port,
                                  '--target', iqn,
-                                 '--target_alias', tgt_alias,
                                  '--initiator', initiator,
-                                 '--initiator_alias', init_alias,
                                  check_exit_code=True)
         LOG.debug('add_iscsi_conn: ' + out + ' -- ' + err)
         return out
 
-    def del_iscsi_conn(self, cmd, ip0, ip1, user, pw, id, lun, ctl, port, iqn,
-                       initiator, force):
+    def del_iscsi_conn(self, cmd, ver, ip0, ip1, user, pw, id, lun, ctl, port,
+                       iqn, initiator, force):
         out, err = utils.execute(cmd,
+                                 '--driver-version', ver,
                                  '--ip0', ip0,
                                  '--ip1', ip1,
                                  '--user', user,
                                  '--password', pw,
-                                 '--delete_iscsi_connection', '1',
-                                 '--array_id', id,
+                                 '--delete-iscsi-connection', '1',
+                                 '--array-id', id,
                                  '--lun', lun,
                                  '--ctl', ctl,
                                  '--port', port,
