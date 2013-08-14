df9c4058f2674968a2f56d04597f5cb2bcc728cd
Walter A. Boring IV <walter.boring@hp.com>
2013-08-14 13:56:09 -0700

    No need to declare the exception conf
    Brick can just assume that the conf entry exists
    in each project that will use brick.
    Cinder and Nova already declare the same exact
    conf entry.
    
    Change-Id: I4e0e11ce6f99a663a75e582ead92ac9d61300032

--- cinder/brick/exception.py
+++ cinder/brick/exception.py
@@ -26,14 +26,7 @@ from cinder.openstack.common import log as logging
 
 LOG = logging.getLogger(__name__)
 
-exc_log_opts = [
-    cfg.BoolOpt('fatal_exception_format_errors',
-                default=False,
-                help='make exception message format errors fatal'),
-]
-
 CONF = cfg.CONF
-CONF.register_opts(exc_log_opts)
 
 
 class BrickException(Exception):
