765e86e72f59994e029ccd1ad5ec3e50170ee128
Chang Bo Guo <guochbo@cn.ibm.com>
2013-09-18 00:00:17 -0700

    Use built-in print() instead of print statement
    In python 3 print statement is not supported, so we should use
    only print() functions.
    
    Fixes bug 1226943
    
    Change-Id: I7b2e4d52fe9050f6a67c44e4cc1237a15ea90b23

--- doc/ext/cinder_autodoc.py
+++ doc/ext/cinder_autodoc.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import gettext
 import os
 
@@ -7,6 +9,6 @@ from cinder import utils
 
 
 def setup(app):
-    print "**Autodocumenting from %s" % os.path.abspath(os.curdir)
+    print("**Autodocumenting from %s" % os.path.abspath(os.curdir))
     rv = utils.execute('./doc/generate_autodoc_index.sh')
-    print rv[0]
+    print(rv[0])
