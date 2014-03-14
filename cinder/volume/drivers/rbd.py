openstack/cinder

commit cbe1d5f5e22e5f792128643e4cdd6afb2ff2b5bf

Remove str() from LOG.* and exceptions
This commit removes the use of str() from LOG.* messages
and exceptions.

The reason we need to remove str() is because str() will fail if
it gets a Unicode string that has something that doesn't translate
to ASCII in it.  If such a situation is encountered you will lose
the message string in question.  In most cases, the use of str() is
unnecessary for LOG.* and exception messages.  Using %s is smart
enough to figure out what to do with what it is passed.  It first
tries to str() it, if this fails it falls back to using unicode.  Either
way, the result will then be something that gettextutils can
handle and translate.

Change-Id: I6eb81043edd9fa5e035d81ee81e8439340546d24
Closes-bug: 1274245
Related-bp: i18n-messages

cinder/volume/drivers/rbd.py
@@ -585,7 +585,7 @@ class RBDDriver(driver.VolumeDriver):
 
     def delete_volume(self, volume):
         """Deletes a logical volume."""
-        volume_name = str(volume['name'])
+        volume_name = volume['name']
         with RADOSClient(self) as client:
             try:
                 rbd_image = self.rbd.Image(client.ioctx, volume_name)
@@ -659,7 +659,7 @@ class RBDDriver(driver.VolumeDriver):
     def delete_snapshot(self, snapshot):
         """Deletes an rbd snapshot."""
         with RBDVolumeProxy(self, snapshot['volume_name']) as volume:
-            snap = str(snapshot['name'])
+            snap = snapshot['name']
             if self._supports_layering():
                 try:
                     volume.unprotect_snap(snap)

cinder/api/contrib/qos_specs_manage.py
@@ -119,23 +119,23 @@ class QoSSpecsController(wsgi.Controller):
                                 'QoSSpecs.create',
                                 notifier_api.INFO, notifier_info)
         except exception.InvalidInput as err:
-            notifier_err = dict(name=name, error_message=str(err))
+            notifier_err = dict(name=name, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.create',
                                          notifier_err)
-            raise webob.exc.HTTPBadRequest(explanation=str(err))
+            raise webob.exc.HTTPBadRequest(explanation=err)
         except exception.QoSSpecsExists as err:
-            notifier_err = dict(name=name, error_message=str(err))
+            notifier_err = dict(name=name, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.create',
                                          notifier_err)
-            raise webob.exc.HTTPConflict(explanation=str(err))
+            raise webob.exc.HTTPConflict(explanation=err)
         except exception.QoSSpecsCreateFailed as err:
-            notifier_err = dict(name=name, error_message=str(err))
+            notifier_err = dict(name=name, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.create',
                                          notifier_err)
-            raise webob.exc.HTTPInternalServerError(explanation=str(err))
+            raise webob.exc.HTTPInternalServerError(explanation=err)
 
         return self._view_builder.detail(req, spec)
 
@@ -154,23 +154,23 @@ class QoSSpecsController(wsgi.Controller):
                                 'qos_specs.update',
                                 notifier_api.INFO, notifier_info)
         except exception.QoSSpecsNotFound as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.update',
                                          notifier_err)
-            raise webob.exc.HTTPNotFound(explanation=str(err))
+            raise webob.exc.HTTPNotFound(explanation=err)
         except exception.InvalidQoSSpecs as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.update',
                                          notifier_err)
-            raise webob.exc.HTTPBadRequest(explanation=str(err))
+            raise webob.exc.HTTPBadRequest(explanation=err)
         except exception.QoSSpecsUpdateFailed as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.update',
                                          notifier_err)
-            raise webob.exc.HTTPInternalServerError(explanation=str(err))
+            raise webob.exc.HTTPInternalServerError(explanation=err)
 
         return body
 
@@ -183,7 +183,7 @@ class QoSSpecsController(wsgi.Controller):
         try:
             spec = qos_specs.get_qos_specs(context, id)
         except exception.QoSSpecsNotFound as err:
-            raise webob.exc.HTTPNotFound(explanation=str(err))
+            raise webob.exc.HTTPNotFound(explanation=err)
 
         return self._view_builder.detail(req, spec)
 
@@ -206,13 +206,13 @@ class QoSSpecsController(wsgi.Controller):
                                 'qos_specs.delete',
                                 notifier_api.INFO, notifier_info)
         except exception.QoSSpecsNotFound as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.delete',
                                          notifier_err)
-            raise webob.exc.HTTPNotFound(explanation=str(err))
+            raise webob.exc.HTTPNotFound(explanation=err)
         except exception.QoSSpecsInUse as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.delete',
                                          notifier_err)
@@ -244,17 +244,17 @@ class QoSSpecsController(wsgi.Controller):
                                 'qos_specs.delete_keys',
                                 notifier_api.INFO, notifier_info)
         except exception.QoSSpecsNotFound as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.delete_keys',
                                          notifier_err)
-            raise webob.exc.HTTPNotFound(explanation=str(err))
+            raise webob.exc.HTTPNotFound(explanation=err)
         except exception.QoSSpecsKeyNotFound as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.delete_keys',
                                          notifier_err)
-            raise webob.exc.HTTPBadRequest(explanation=str(err))
+            raise webob.exc.HTTPBadRequest(explanation=err)
 
         return webob.Response(status_int=202)
 
@@ -273,17 +273,17 @@ class QoSSpecsController(wsgi.Controller):
                                 'qos_specs.associations',
                                 notifier_api.INFO, notifier_info)
         except exception.QoSSpecsNotFound as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.associations',
                                          notifier_err)
-            raise webob.exc.HTTPNotFound(explanation=str(err))
+            raise webob.exc.HTTPNotFound(explanation=err)
         except exception.CinderException as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.associations',
                                          notifier_err)
-            raise webob.exc.HTTPInternalServerError(explanation=str(err))
+            raise webob.exc.HTTPInternalServerError(explanation=err)
 
         return self._view_builder.associations(req, associates)
 
@@ -311,32 +311,32 @@ class QoSSpecsController(wsgi.Controller):
                                 'qos_specs.associate',
                                 notifier_api.INFO, notifier_info)
         except exception.VolumeTypeNotFound as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.associate',
                                          notifier_err)
-            raise webob.exc.HTTPNotFound(explanation=str(err))
+            raise webob.exc.HTTPNotFound(explanation=err)
         except exception.QoSSpecsNotFound as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.associate',
                                          notifier_err)
-            raise webob.exc.HTTPNotFound(explanation=str(err))
+            raise webob.exc.HTTPNotFound(explanation=err)
         except exception.InvalidVolumeType as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.associate',
                                          notifier_err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.associate',
                                          notifier_err)
-            raise webob.exc.HTTPBadRequest(explanation=str(err))
+            raise webob.exc.HTTPBadRequest(explanation=err)
         except exception.QoSSpecsAssociateFailed as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.associate',
                                          notifier_err)
-            raise webob.exc.HTTPInternalServerError(explanation=str(err))
+            raise webob.exc.HTTPInternalServerError(explanation=err)
 
         return webob.Response(status_int=202)
 
@@ -364,23 +364,23 @@ class QoSSpecsController(wsgi.Controller):
                                 'qos_specs.disassociate',
                                 notifier_api.INFO, notifier_info)
         except exception.VolumeTypeNotFound as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.disassociate',
                                          notifier_err)
-            raise webob.exc.HTTPNotFound(explanation=str(err))
+            raise webob.exc.HTTPNotFound(explanation=err)
         except exception.QoSSpecsNotFound as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.disassociate',
                                          notifier_err)
-            raise webob.exc.HTTPNotFound(explanation=str(err))
+            raise webob.exc.HTTPNotFound(explanation=err)
         except exception.QoSSpecsDisassociateFailed as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.disassociate',
                                          notifier_err)
-            raise webob.exc.HTTPInternalServerError(explanation=str(err))
+            raise webob.exc.HTTPInternalServerError(explanation=err)
 
         return webob.Response(status_int=202)
 
@@ -398,17 +398,17 @@ class QoSSpecsController(wsgi.Controller):
                                 'qos_specs.disassociate_all',
                                 notifier_api.INFO, notifier_info)
         except exception.QoSSpecsNotFound as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.disassociate_all',
                                          notifier_err)
-            raise webob.exc.HTTPNotFound(explanation=str(err))
+            raise webob.exc.HTTPNotFound(explanation=err)
         except exception.QoSSpecsDisassociateFailed as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_qos_specs_error(context,
                                          'qos_specs.disassociate_all',
                                          notifier_err)
-            raise webob.exc.HTTPInternalServerError(explanation=str(err))
+            raise webob.exc.HTTPInternalServerError(explanation=err)
 
         return webob.Response(status_int=202)
 

cinder/api/contrib/types_manage.py
@@ -67,14 +67,14 @@ class VolumeTypesManageController(wsgi.Controller):
                                 notifier_api.INFO, notifier_info)
 
         except exception.VolumeTypeExists as err:
-            notifier_err = dict(volume_types=vol_type, error_message=str(err))
+            notifier_err = dict(volume_types=vol_type, error_message=err)
             self._notify_volume_type_error(context,
                                            'volume_type.create',
                                            notifier_err)
 
-            raise webob.exc.HTTPConflict(explanation=str(err))
+            raise webob.exc.HTTPConflict(explanation=err)
         except exception.NotFound as err:
-            notifier_err = dict(volume_types=vol_type, error_message=str(err))
+            notifier_err = dict(volume_types=vol_type, error_message=err)
             self._notify_volume_type_error(context,
                                            'volume_type.create',
                                            notifier_err)
@@ -96,14 +96,14 @@ class VolumeTypesManageController(wsgi.Controller):
                                 'volume_type.delete',
                                 notifier_api.INFO, notifier_info)
         except exception.VolumeTypeInUse as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_volume_type_error(context,
                                            'volume_type.delete',
                                            notifier_err)
             msg = 'Target volume type is still in use.'
             raise webob.exc.HTTPBadRequest(explanation=msg)
         except exception.NotFound as err:
-            notifier_err = dict(id=id, error_message=str(err))
+            notifier_err = dict(id=id, error_message=err)
             self._notify_volume_type_error(context,
                                            'volume_type.delete',
                                            notifier_err)

cinder/backup/drivers/ceph.py
@@ -814,7 +814,7 @@ class CephBackupDriver(BackupDriver):
                 vol_meta_backup = VolumeMetadataBackup(client, backup['id'])
                 vol_meta_backup.set(json_meta)
         except exception.VolumeMetadataBackupExists as e:
-            msg = _("Failed to backup volume metadata - %s") % (str(e))
+            msg = (_("Failed to backup volume metadata - %s") % (e))
             raise exception.BackupOperationError(msg)
 
     def backup(self, backup, volume_file, backup_metadata=True):

cinder/backup/drivers/swift.py
@@ -33,11 +33,11 @@
 import hashlib
 import json
 import os
+import six
 import socket
 
 import eventlet
 from oslo.config import cfg
-import six
 
 from cinder.backup.driver import BackupDriver
 from cinder import exception
@@ -225,7 +225,7 @@ class SwiftBackupDriver(BackupDriver):
         try:
             container = self._create_container(self.context, backup)
         except socket.error as err:
-            raise exception.SwiftConnectionFailed(reason=str(err))
+            raise exception.SwiftConnectionFailed(reason=err)
 
         object_prefix = self._generate_swift_object_name_prefix(backup)
         backup['service_metadata'] = object_prefix
@@ -282,7 +282,7 @@ class SwiftBackupDriver(BackupDriver):
             etag = self.conn.put_object(container, object_name, reader,
                                         content_length=len(data))
         except socket.error as err:
-            raise exception.SwiftConnectionFailed(reason=str(err))
+            raise exception.SwiftConnectionFailed(reason=err)
         LOG.debug(_('swift MD5 for %(object_name)s: %(etag)s') %
                   {'object_name': object_name, 'etag': etag, })
         md5 = hashlib.md5(data).hexdigest()
@@ -313,7 +313,7 @@ class SwiftBackupDriver(BackupDriver):
                                  object_list,
                                  volume_meta)
         except socket.error as err:
-            raise exception.SwiftConnectionFailed(reason=str(err))
+            raise exception.SwiftConnectionFailed(reason=err)
         self.db.backup_update(self.context, backup['id'],
                               {'object_count': object_id})
         LOG.debug(_('backup %s finished.') % backup['id'])
@@ -388,7 +388,7 @@ class SwiftBackupDriver(BackupDriver):
             try:
                 (resp, body) = self.conn.get_object(container, object_name)
             except socket.error as err:
-                raise exception.SwiftConnectionFailed(reason=str(err))
+                raise exception.SwiftConnectionFailed(reason=err)
             compression_algorithm = metadata_object[object_name]['compression']
             decompressor = self._get_compressor(compression_algorithm)
             if decompressor is not None:
@@ -435,7 +435,7 @@ class SwiftBackupDriver(BackupDriver):
         try:
             metadata = self._read_metadata(backup)
         except socket.error as err:
-            raise exception.SwiftConnectionFailed(reason=str(err))
+            raise exception.SwiftConnectionFailed(reason=err)
         metadata_version = metadata['version']
         LOG.debug(_('Restoring swift backup version %s'), metadata_version)
         try:
@@ -479,7 +479,7 @@ class SwiftBackupDriver(BackupDriver):
                 try:
                     self.conn.delete_object(container, swift_object_name)
                 except socket.error as err:
-                    raise exception.SwiftConnectionFailed(reason=str(err))
+                    raise exception.SwiftConnectionFailed(reason=err)
                 except Exception:
                     LOG.warn(_('swift error while deleting object %s, '
                                'continuing with delete') % swift_object_name)

cinder/brick/iscsi/iscsi.py
@@ -27,7 +27,6 @@ import time
 from cinder.brick import exception
 from cinder.brick import executor
 from cinder.openstack.common import fileutils
-from cinder.openstack.common.gettextutils import _
 from cinder.openstack.common import log as logging
 from cinder.openstack.common import processutils as putils
 
@@ -154,7 +153,7 @@ class TgtAdm(TargetAdmin):
             LOG.error(_("Failed to recover attempt to create "
                         "iscsi backing lun for volume "
                         "id:%(vol_id)s: %(e)s")
-                      % {'vol_id': name, 'e': str(e)})
+                      % {'vol_id': name, 'e': e})
 
     def create_iscsi_target(self, name, tid, lun, path,
                             chap_auth=None, **kwargs):
@@ -211,7 +210,7 @@ class TgtAdm(TargetAdmin):
         except putils.ProcessExecutionError as e:
             LOG.warning(_("Failed to create iscsi target for volume "
                         "id:%(vol_id)s: %(e)s")
-                        % {'vol_id': vol_id, 'e': str(e)})
+                        % {'vol_id': vol_id, 'e': e})
 
             #Don't forget to remove the persistent file we created
             os.unlink(volume_path)
@@ -274,7 +273,7 @@ class TgtAdm(TargetAdmin):
         except putils.ProcessExecutionError as e:
             LOG.error(_("Failed to remove iscsi target for volume "
                         "id:%(vol_id)s: %(e)s")
-                      % {'vol_id': vol_id, 'e': str(e)})
+                      % {'vol_id': vol_id, 'e': e})
             raise exception.ISCSITargetRemoveFailed(volume_id=vol_id)
 
         # NOTE(jdg): This *should* be there still but incase
@@ -367,7 +366,7 @@ class IetAdm(TargetAdmin):
                 vol_id = name.split(':')[1]
                 LOG.error(_("Failed to create iscsi target for volume "
                             "id:%(vol_id)s: %(e)s")
-                          % {'vol_id': vol_id, 'e': str(e)})
+                          % {'vol_id': vol_id, 'e': e})
                 raise exception.ISCSITargetCreateFailed(volume_id=vol_id)
         return tid
 
@@ -512,7 +511,7 @@ class LioAdm(TargetAdmin):
         except putils.ProcessExecutionError as e:
             LOG.error(_("Failed to create iscsi target for volume "
                         "id:%s.") % vol_id)
-            LOG.error("%s" % str(e))
+            LOG.error("%s" % e)
 
             raise exception.ISCSITargetCreateFailed(volume_id=vol_id)
 
@@ -538,7 +537,7 @@ class LioAdm(TargetAdmin):
         except putils.ProcessExecutionError as e:
             LOG.error(_("Failed to remove iscsi target for volume "
                         "id:%s.") % vol_id)
-            LOG.error("%s" % str(e))
+            LOG.error("%s" % e)
             raise exception.ISCSITargetRemoveFailed(volume_id=vol_id)
 
     def show_target(self, tid, iqn=None, **kwargs):

cinder/image/glance.py
@@ -173,7 +173,7 @@ class GlanceClientWrapper(object):
                                                    'extra': extra,
                                                    }
                     LOG.exception(error_msg)
-                    raise exception.GlanceConnectionFailed(reason=str(e))
+                    raise exception.GlanceConnectionFailed(reason=e)
                 LOG.exception(error_msg)
                 time.sleep(1)
 

cinder/test.py
@@ -225,8 +225,8 @@ class TestCase(testtools.TestCase):
 
         """
         def raise_assertion(msg):
-            d1str = str(d1)
-            d2str = str(d2)
+            d1str = d1
+            d2str = d2
             base_msg = ('Dictionaries do not match. %(msg)s d1: %(d1str)s '
                         'd2: %(d2str)s' %
                         {'msg': msg, 'd1str': d1str, 'd2str': d2str})

commit c0f8cc39fc92517d1402d9eb82d9319c6cb72842

Ensure name is utf-8 when deleting rbd vol or snap
If the name supplied to librbd is not utf-8 e.g. unicode,
an exception is raised. This was previously fixed and
subsequently broken by commit cbe1d5f.

Change-Id: I29f5e9db87ceb5f9cfe3be43e76d3e860edaf6d7
Closes-Bug: bug 1292433

cinder/volume/drivers/rbd.py
@@ -26,6 +26,7 @@ from cinder import exception
 from cinder.image import image_utils
 from cinder.openstack.common import fileutils
 from cinder.openstack.common import log as logging
+from cinder.openstack.common import strutils
 from cinder import units
 from cinder.volume import driver
 
@@ -585,7 +586,9 @@ class RBDDriver(driver.VolumeDriver):
 
     def delete_volume(self, volume):
         """Deletes a logical volume."""
-        volume_name = volume['name']
+        # NOTE(dosaboy): this was broken by commit cbe1d5f. Ensure names are
+        #                utf-8 otherwise librbd will barf.
+        volume_name = strutils.safe_encode(volume['name'])
         with RADOSClient(self) as client:
             try:
                 rbd_image = self.rbd.Image(client.ioctx, volume_name)
@@ -658,14 +661,17 @@ class RBDDriver(driver.VolumeDriver):
 
     def delete_snapshot(self, snapshot):
         """Deletes an rbd snapshot."""
-        with RBDVolumeProxy(self, snapshot['volume_name']) as volume:
-            snap = snapshot['name']
+        # NOTE(dosaboy): this was broken by commit cbe1d5f. Ensure names are
+        #                utf-8 otherwise librbd will barf.
+        volume_name = strutils.safe_encode(snapshot['volume_name'])
+        snap_name = strutils.safe_encode(snapshot['name'])
+        with RBDVolumeProxy(self, volume_name) as volume:
             if self._supports_layering():
                 try:
-                    volume.unprotect_snap(snap)
+                    volume.unprotect_snap(snap_name)
                 except self.rbd.ImageBusy:
-                    raise exception.SnapshotIsBusy(snapshot_name=snap)
-            volume.remove_snap(snap)
+                    raise exception.SnapshotIsBusy(snapshot_name=snap_name)
+            volume.remove_snap(snap_name)
 
     def ensure_export(self, context, volume):
         """Synchronously recreates an export for a logical volume."""