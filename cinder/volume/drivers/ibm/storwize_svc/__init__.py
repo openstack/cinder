0814bff9366974d77d42d03fa11f60fbe469a8c7
Dirk Mueller <dirk@dmllr.de>
2014-04-14 17:12:23 +0200

    Switch over to FixedIntervalLoopingCall
    Change-Id: Iebe5269ea01acdd4ba011742831ca5e569f66b28
    Closes-Bug: #1307560

--- cinder/volume/drivers/ibm/storwize_svc/__init__.py
+++ cinder/volume/drivers/ibm/storwize_svc/__init__.py
@@ -207,7 +207,7 @@ class StorwizeSVCDriver(san.SanDriver):
 
         # if vdiskcopy exists in database, start the looping call
         if len(self._vdiskcopyops) >= 1:
-            self._vdiskcopyops_loop = loopingcall.LoopingCall(
+            self._vdiskcopyops_loop = loopingcall.FixedIntervalLoopingCall(
                 self._check_volume_copy_ops)
             self._vdiskcopyops_loop.start(interval=self.VDISKCOPYOPS_INTERVAL)
 
@@ -567,7 +567,7 @@ class StorwizeSVCDriver(san.SanDriver):
 
         # We added the first copy operation, so start the looping call
         if len(self._vdiskcopyops) == 1:
-            self._vdiskcopyops_loop = loopingcall.LoopingCall(
+            self._vdiskcopyops_loop = loopingcall.FixedIntervalLoopingCall(
                 self._check_volume_copy_ops)
             self._vdiskcopyops_loop.start(interval=self.VDISKCOPYOPS_INTERVAL)
 
