openstack/cinder

commit 0505bb268942534ad5d6ecd5e34a4d9b0e7f5c04

Remove second get call to list/show volumes
Recently it was observed that with large numbers of volumes
that things like "cinder list" could take extremely long to
return.

For example, running cinder list on a system with 1000 volumes took
greater than 30 seconds to return.  It turns out that the cause of
this is the addition of visible admin_metadata.

There's two problems with this:
1. The original patch probably shouldn't have gone in
   data is either admin data or it's not, selectively picking
   pieces of admin data out to provide to the user just creates
   complications and introduces confusion.
2. The REAL issue here is that since the standar gets are made
   with the standard user context, the add_visible_admin_metadata
   would go through and do an elevated context get on every single
   volume individually.  This is what caused the horrible performance
   issue on cinder list with large numbers of volumes.

Running as admin, or removing the second call drops this down to about 3
seconds for the same 1000 volume list.

This patch removes the secondary admin context get_call.  Instead where
we expect to do display the visible admin_meta, we pass in a flag
requesting that the volume object we're getting has the appropriate
metadata.  This way we can elevate the context if needed and avoid
iterating through the gets again.

This patch also cleans up the get_visible_admin_meta methods, and
consolidates both V1 and V2 to use the utils method.

Change-Id: I3fb7aefb7d8a5664b0a3fb3958f509b5cd621320
Closes-Bug: 1317606

cinder/volume/api.py
@@ -260,14 +260,16 @@ class API(base.Base):
     def update(self, context, volume, fields):
         self.db.volume_update(context, volume['id'], fields)
 
-    def get(self, context, volume_id):
+    def get(self, context, volume_id, viewable_admin_meta=False):
+        if viewable_admin_meta:
+            context = context.elevated()
         rv = self.db.volume_get(context, volume_id)
         volume = dict(rv.iteritems())
         check_policy(context, 'get', volume)
         return volume
 
     def get_all(self, context, marker=None, limit=None, sort_key='created_at',
-                sort_dir='desc', filters=None):
+                sort_dir='desc', filters=None, viewable_admin_meta=False):
         check_policy(context, 'get_all')
         if filters == None:
             filters = {}
@@ -298,6 +300,8 @@ class API(base.Base):
             volumes = self.db.volume_get_all(context, marker, limit, sort_key,
                                              sort_dir, filters=filters)
         else:
+            if viewable_admin_meta:
+                context = context.elevated()
             volumes = self.db.volume_get_all_by_project(context,
                                                         context.project_id,
                                                         marker, limit,

cinder/tests/api/v2/test_volumes.py
@@ -106,8 +106,7 @@ class VolumeApiTest(test.TestCase):
                            'rel': 'self'},
                           {'href': 'http://localhost/fake/volumes/1',
                            'rel': 'bookmark'}],
-                         'metadata': {'attached_mode': 'rw',
-                                      'readonly': 'False'},
+                         'metadata': {},
                          'name': 'Volume Test Name',
                          'size': 100,
                          'snapshot_id': None,
@@ -210,8 +209,7 @@ class VolumeApiTest(test.TestCase):
                            'rel': 'self'},
                           {'href': 'http://localhost/fake/volumes/1',
                            'rel': 'bookmark'}],
-                         'metadata': {'attached_mode': 'rw',
-                                      'readonly': 'False'},
+                         'metadata': {},
                          'name': 'Volume Test Name',
                          'size': '1',
                          'snapshot_id': None,
@@ -706,7 +704,8 @@ class VolumeApiTest(test.TestCase):
 
     def test_volume_index_with_marker(self):
         def stub_volume_get_all_by_project(context, project_id, marker, limit,
-                                           sort_key, sort_dir, filters=None):
+                                           sort_key, sort_dir, filters=None,
+                                           viewable_admin_meta=False):
             return [
                 stubs.stub_volume(1, display_name='vol1'),
                 stubs.stub_volume(2, display_name='vol2'),
@@ -766,7 +765,8 @@ class VolumeApiTest(test.TestCase):
 
     def test_volume_index_limit_offset(self):
         def stub_volume_get_all_by_project(context, project_id, marker, limit,
-                                           sort_key, sort_dir, filters=None):
+                                           sort_key, sort_dir, filters=None,
+                                           viewable_admin_meta=False):
             return [
                 stubs.stub_volume(1, display_name='vol1'),
                 stubs.stub_volume(2, display_name='vol2'),
@@ -793,7 +793,8 @@ class VolumeApiTest(test.TestCase):
 
     def test_volume_detail_with_marker(self):
         def stub_volume_get_all_by_project(context, project_id, marker, limit,
-                                           sort_key, sort_dir, filters=None):
+                                           sort_key, sort_dir, filters=None,
+                                           viewable_admin_meta=False):
             return [
                 stubs.stub_volume(1, display_name='vol1'),
                 stubs.stub_volume(2, display_name='vol2'),
@@ -853,7 +854,8 @@ class VolumeApiTest(test.TestCase):
 
     def test_volume_detail_limit_offset(self):
         def stub_volume_get_all_by_project(context, project_id, marker, limit,
-                                           sort_key, sort_dir, filters=None):
+                                           sort_key, sort_dir, filters=None,
+                                           viewable_admin_meta=False):
             return [
                 stubs.stub_volume(1, display_name='vol1'),
                 stubs.stub_volume(2, display_name='vol2'),
@@ -887,7 +889,7 @@ class VolumeApiTest(test.TestCase):
 
     def test_volume_with_limit_zero(self):
         def stub_volume_get_all(context, marker, limit,
-                                sort_key, sort_dir):
+                                sort_key, sort_dir, **kwargs):
             return []
         self.stubs.Set(db, 'volume_get_all', stub_volume_get_all)
         req = fakes.HTTPRequest.blank('/v2/volumes?limit=0')
@@ -911,7 +913,8 @@ class VolumeApiTest(test.TestCase):
         # Number of volumes equals the max, include next link
         def stub_volume_get_all(context, marker, limit,
                                 sort_key, sort_dir,
-                                filters=None):
+                                filters=None,
+                                viewable_admin_meta=False):
             vols = [stubs.stub_volume(i)
                     for i in xrange(CONF.osapi_max_limit)]
             if limit == None or limit >= len(vols):
@@ -929,7 +932,8 @@ class VolumeApiTest(test.TestCase):
         # Number of volumes less then max, do not include
         def stub_volume_get_all2(context, marker, limit,
                                  sort_key, sort_dir,
-                                 filters=None):
+                                 filters=None,
+                                 viewable_admin_meta=False):
             vols = [stubs.stub_volume(i)
                     for i in xrange(100)]
             if limit == None or limit >= len(vols):
@@ -946,7 +950,8 @@ class VolumeApiTest(test.TestCase):
         # Number of volumes more then the max, include next link
         def stub_volume_get_all3(context, marker, limit,
                                  sort_key, sort_dir,
-                                 filters=None):
+                                 filters=None,
+                                 viewable_admin_meta=False):
             vols = [stubs.stub_volume(i)
                     for i in xrange(CONF.osapi_max_limit + 100)]
             if limit == None or limit >= len(vols):
@@ -985,13 +990,15 @@ class VolumeApiTest(test.TestCase):
         """
         # Non-admin, project function should be called with no_migration_status
         def stub_volume_get_all_by_project(context, project_id, marker, limit,
-                                           sort_key, sort_dir, filters=None):
+                                           sort_key, sort_dir, filters=None,
+                                           viewable_admin_meta=False):
             self.assertEqual(filters['no_migration_targets'], True)
             self.assertFalse('all_tenants' in filters)
             return [stubs.stub_volume(1, display_name='vol1')]
 
         def stub_volume_get_all(context, marker, limit,
-                                sort_key, sort_dir, filters=None):
+                                sort_key, sort_dir, filters=None,
+                                viewable_admin_meta=False):
             return []
         self.stubs.Set(db, 'volume_get_all_by_project',
                        stub_volume_get_all_by_project)
@@ -1007,12 +1014,14 @@ class VolumeApiTest(test.TestCase):
         # Admin, all_tenants is not set, project function should be called
         # without no_migration_status
         def stub_volume_get_all_by_project2(context, project_id, marker, limit,
-                                            sort_key, sort_dir, filters=None):
+                                            sort_key, sort_dir, filters=None,
+                                            viewable_admin_meta=False):
             self.assertFalse('no_migration_targets' in filters)
             return [stubs.stub_volume(1, display_name='vol2')]
 
         def stub_volume_get_all2(context, marker, limit,
-                                 sort_key, sort_dir, filters=None):
+                                 sort_key, sort_dir, filters=None,
+                                 viewable_admin_meta=False):
             return []
         self.stubs.Set(db, 'volume_get_all_by_project',
                        stub_volume_get_all_by_project2)
@@ -1026,11 +1035,13 @@ class VolumeApiTest(test.TestCase):
         # Admin, all_tenants is set, get_all function should be called
         # without no_migration_status
         def stub_volume_get_all_by_project3(context, project_id, marker, limit,
-                                            sort_key, sort_dir, filters=None):
+                                            sort_key, sort_dir, filters=None,
+                                            viewable_admin_meta=False):
             return []
 
         def stub_volume_get_all3(context, marker, limit,
-                                 sort_key, sort_dir, filters=None):
+                                 sort_key, sort_dir, filters=None,
+                                 viewable_admin_meta=False):
             self.assertFalse('no_migration_targets' in filters)
             self.assertFalse('all_tenants' in filters)
             return [stubs.stub_volume(1, display_name='vol3')]
@@ -1091,7 +1102,7 @@ class VolumeApiTest(test.TestCase):
         self.assertIsNotNone(req.cached_resource_by_id('1'))
 
     def test_volume_show_no_attachments(self):
-        def stub_volume_get(self, context, volume_id):
+        def stub_volume_get(self, context, volume_id, **kwargs):
             return stubs.stub_volume(volume_id, attach_status='detached')
 
         self.stubs.Set(volume_api.API, 'get', stub_volume_get)
@@ -1196,7 +1207,7 @@ class VolumeApiTest(test.TestCase):
         self.assertEqual(res_dict, expected)
 
     def test_volume_show_with_encrypted_volume(self):
-        def stub_volume_get(self, context, volume_id):
+        def stub_volume_get(self, context, volume_id, **kwargs):
             return stubs.stub_volume(volume_id, encryption_key_id='fake_id')
 
         self.stubs.Set(volume_api.API, 'get', stub_volume_get)
@@ -1206,7 +1217,7 @@ class VolumeApiTest(test.TestCase):
         self.assertEqual(res_dict['volume']['encrypted'], True)
 
     def test_volume_show_with_unencrypted_volume(self):
-        def stub_volume_get(self, context, volume_id):
+        def stub_volume_get(self, context, volume_id, **kwargs):
             return stubs.stub_volume(volume_id, encryption_key_id=None)
 
         self.stubs.Set(volume_api.API, 'get', stub_volume_get)
@@ -1306,9 +1317,7 @@ class VolumeApiTest(test.TestCase):
         metadata = [{"key": "key", "value": "value"}]
         volume = dict(volume_admin_metadata=admin_metadata,
                       volume_metadata=metadata)
-        admin_ctx = context.get_admin_context()
-        utils.add_visible_admin_metadata(admin_ctx, volume,
-                                         self.controller.volume_api)
+        utils.add_visible_admin_metadata(volume)
 
         self.assertEqual(volume['volume_metadata'],
                          [{"key": "key", "value": "value"},
@@ -1321,9 +1330,7 @@ class VolumeApiTest(test.TestCase):
         metadata = {"key": "value"}
         volume = dict(admin_metadata=admin_metadata,
                       metadata=metadata)
-        admin_ctx = context.get_admin_context()
-        utils.add_visible_admin_metadata(admin_ctx, volume,
-                                         self.controller.volume_api)
+        utils.add_visible_admin_metadata(volume)
         self.assertEqual(volume['metadata'],
                          {'key': 'value',
                           'attached_mode': 'visible',

commit 5868e8f285d23b56ca6123dab760342c57bf8c80

Prevent tenant viewing volumes owed by another
Bug introduced by 0505bb268942534ad5d6ecd5e34a4d9b0e7f5c04 allows any
tenant to get the details of a volume belonging to any other tenant
if the UUID is known.

This commit allows only the tenant or an admin to get a volume.

Change-Id: I0268d374f7529d89068dcbf3c1cb9ab3d60d4115
Closes-Bug: #1356368

cinder/volume/api.py
@@ -283,9 +283,13 @@ class API(base.Base):
 
     def get(self, context, volume_id, viewable_admin_meta=False):
         if viewable_admin_meta:
-            context = context.elevated()
-        rv = self.db.volume_get(context, volume_id)
+            ctxt = context.elevated()
+        else:
+            ctxt = context
+        rv = self.db.volume_get(ctxt, volume_id)
         volume = dict(rv.iteritems())
+        if not context.is_admin and volume['project_id'] != context.project_id:
+            raise exception.VolumeNotFound(volume_id=volume_id)
         check_policy(context, 'get', volume)
         return volume
 

cinder/tests/api/fakes.py
@@ -115,7 +115,7 @@ class HTTPRequest(webob.Request):
         out = os_wsgi.Request.blank(*args, **kwargs)
         out.environ['cinder.context'] = FakeRequestContext(
             'fake_user',
-            'fake',
+            'fakeproject',
             is_admin=use_admin_context)
         return out
 

cinder/tests/api/test_router.py
@@ -224,35 +224,35 @@ class VolumeRouterTestCase(test.TestCase):
         self.assertEqual(set(ids), set(['v1.0']))
 
     def test_volumes(self):
-        req = fakes.HTTPRequest.blank('/fake/volumes')
+        req = fakes.HTTPRequest.blank('/fakeproject/volumes')
         req.method = 'GET'
         req.content_type = 'application/json'
         response = req.get_response(self.app)
         self.assertEqual(200, response.status_int)
 
     def test_volumes_detail(self):
-        req = fakes.HTTPRequest.blank('/fake/volumes/detail')
+        req = fakes.HTTPRequest.blank('/fakeproject/volumes/detail')
         req.method = 'GET'
         req.content_type = 'application/json'
         response = req.get_response(self.app)
         self.assertEqual(200, response.status_int)
 
     def test_types(self):
-        req = fakes.HTTPRequest.blank('/fake/types')
+        req = fakes.HTTPRequest.blank('/fakeproject/types')
         req.method = 'GET'
         req.content_type = 'application/json'
         response = req.get_response(self.app)
         self.assertEqual(200, response.status_int)
 
     def test_snapshots(self):
-        req = fakes.HTTPRequest.blank('/fake/snapshots')
+        req = fakes.HTTPRequest.blank('/fakeproject/snapshots')
         req.method = 'GET'
         req.content_type = 'application/json'
         response = req.get_response(self.app)
         self.assertEqual(200, response.status_int)
 
     def test_snapshots_detail(self):
-        req = fakes.HTTPRequest.blank('/fake/snapshots/detail')
+        req = fakes.HTTPRequest.blank('/fakeproject/snapshots/detail')
         req.method = 'GET'
         req.content_type = 'application/json'
         response = req.get_response(self.app)

cinder/tests/api/v1/test_snapshot_metadata.py
@@ -121,7 +121,8 @@ def return_volume(context, volume_id):
             'encryption_key_id': None,
             'volume_type_id': None,
             'migration_status': None,
-            'metadata': {}}
+            'metadata': {},
+            'project_id': context.project_id}
 
 
 def return_snapshot_nonexistent(context, snapshot_id):

cinder/tests/api/v1/test_volume_metadata.py
@@ -106,7 +106,8 @@ def stub_max_volume_metadata():
 def return_volume(context, volume_id):
     return {'id': '0cc3346e-9fef-4445-abe6-5d2b2690ec64',
             'name': 'fake',
-            'metadata': {}}
+            'metadata': {},
+            'project_id': context.project_id}
 
 
 def return_volume_nonexistent(context, volume_id):

cinder/tests/api/v2/test_snapshot_metadata.py
@@ -121,7 +121,8 @@ def return_volume(context, volume_id):
             'encryption_key_id': None,
             'volume_type_id': None,
             'migration_status': None,
-            'metadata': {}}
+            'metadata': {},
+            'project_id': context.project_id}
 
 
 def return_snapshot_nonexistent(context, snapshot_id):

cinder/tests/api/v2/test_volume_metadata.py
@@ -107,7 +107,8 @@ def stub_max_volume_metadata():
 def return_volume(context, volume_id):
     return {'id': '0cc3346e-9fef-4445-abe6-5d2b2690ec64',
             'name': 'fake',
-            'metadata': {}}
+            'metadata': {},
+            'project_id': context.project_id}
 
 
 def return_volume_nonexistent(context, volume_id):

cinder/tests/api/v2/test_volumes.py
@@ -98,9 +98,9 @@ class VolumeApiTest(test.TestCase):
                          'description': 'Volume Test Desc',
                          'id': '1',
                          'links':
-                         [{'href': 'http://localhost/v2/fake/volumes/1',
+                         [{'href': 'http://localhost/v2/fakeproject/volumes/1',
                            'rel': 'self'},
-                          {'href': 'http://localhost/fake/volumes/1',
+                          {'href': 'http://localhost/fakeproject/volumes/1',
                            'rel': 'bookmark'}],
                          'metadata': {},
                          'name': 'Volume Test Name',
@@ -201,9 +201,9 @@ class VolumeApiTest(test.TestCase):
                          'encrypted': False,
                          'id': '1',
                          'links':
-                         [{'href': 'http://localhost/v2/fake/volumes/1',
+                         [{'href': 'http://localhost/v2/fakeproject/volumes/1',
                            'rel': 'self'},
-                          {'href': 'http://localhost/fake/volumes/1',
+                          {'href': 'http://localhost/fakeproject/volumes/1',
                            'rel': 'bookmark'}],
                          'metadata': {},
                          'name': 'Volume Test Name',
@@ -305,11 +305,11 @@ class VolumeApiTest(test.TestCase):
                 'size': 1,
                 'links': [
                     {
-                        'href': 'http://localhost/v2/fake/volumes/1',
+                        'href': 'http://localhost/v2/fakeproject/volumes/1',
                         'rel': 'self'
                     },
                     {
-                        'href': 'http://localhost/fake/volumes/1',
+                        'href': 'http://localhost/fakeproject/volumes/1',
                         'rel': 'bookmark'
                     }
                 ],
@@ -357,11 +357,11 @@ class VolumeApiTest(test.TestCase):
                 'size': 1,
                 'links': [
                     {
-                        'href': 'http://localhost/v2/fake/volumes/1',
+                        'href': 'http://localhost/v2/fakeproject/volumes/1',
                         'rel': 'self'
                     },
                     {
-                        'href': 'http://localhost/fake/volumes/1',
+                        'href': 'http://localhost/fakeproject/volumes/1',
                         'rel': 'bookmark'
                     }
                 ],
@@ -412,11 +412,11 @@ class VolumeApiTest(test.TestCase):
                 'size': 1,
                 'links': [
                     {
-                        'href': 'http://localhost/v2/fake/volumes/1',
+                        'href': 'http://localhost/v2/fakeproject/volumes/1',
                         'rel': 'self'
                     },
                     {
-                        'href': 'http://localhost/fake/volumes/1',
+                        'href': 'http://localhost/fakeproject/volumes/1',
                         'rel': 'bookmark'
                     }
                 ],
@@ -462,11 +462,11 @@ class VolumeApiTest(test.TestCase):
             'size': 1,
             'links': [
                 {
-                    'href': 'http://localhost/v2/fake/volumes/1',
+                    'href': 'http://localhost/v2/fakeproject/volumes/1',
                     'rel': 'self'
                 },
                 {
-                    'href': 'http://localhost/fake/volumes/1',
+                    'href': 'http://localhost/fakeproject/volumes/1',
                     'rel': 'bookmark'
                 }
             ],
@@ -575,11 +575,12 @@ class VolumeApiTest(test.TestCase):
                     'id': '1',
                     'links': [
                         {
-                            'href': 'http://localhost/v2/fake/volumes/1',
+                            'href': 'http://localhost/v2/fakeproject/volumes/'
+                                    '1',
                             'rel': 'self'
                         },
                         {
-                            'href': 'http://localhost/fake/volumes/1',
+                            'href': 'http://localhost/fakeproject/volumes/1',
                             'rel': 'bookmark'
                         }
                     ],
@@ -625,11 +626,12 @@ class VolumeApiTest(test.TestCase):
                     'size': 1,
                     'links': [
                         {
-                            'href': 'http://localhost/v2/fake/volumes/1',
+                            'href': 'http://localhost/v2/fakeproject/volumes/'
+                                    '1',
                             'rel': 'self'
                         },
                         {
-                            'href': 'http://localhost/fake/volumes/1',
+                            'href': 'http://localhost/fakeproject/volumes/1',
                             'rel': 'bookmark'
                         }
                     ],
@@ -731,7 +733,7 @@ class VolumeApiTest(test.TestCase):
         links = res_dict['volumes_links']
         self.assertEqual(links[0]['rel'], 'next')
         href_parts = urlparse.urlparse(links[0]['href'])
-        self.assertEqual('/v2/fake/volumes', href_parts.path)
+        self.assertEqual('/v2/fakeproject/volumes', href_parts.path)
         params = urlparse.parse_qs(href_parts.query)
         self.assertTrue('marker' in params)
         self.assertEqual('1', params['limit'][0])
@@ -820,7 +822,7 @@ class VolumeApiTest(test.TestCase):
         links = res_dict['volumes_links']
         self.assertEqual(links[0]['rel'], 'next')
         href_parts = urlparse.urlparse(links[0]['href'])
-        self.assertEqual('/v2/fake/volumes/detail', href_parts.path)
+        self.assertEqual('/v2/fakeproject/volumes/detail', href_parts.path)
         params = urlparse.parse_qs(href_parts.query)
         self.assertTrue('marker' in params)
         self.assertEqual('1', params['limit'][0])
@@ -900,7 +902,7 @@ class VolumeApiTest(test.TestCase):
             '''Verify next link and url.'''
             self.assertEqual(links[0]['rel'], 'next')
             href_parts = urlparse.urlparse(links[0]['href'])
-            self.assertEqual('/v2/fake/%s' % key, href_parts.path)
+            self.assertEqual('/v2/fakeproject/%s' % key, href_parts.path)
 
         # Verify both the index and detail queries
         api_keys = ['volumes', 'volumes/detail']
@@ -1083,11 +1085,11 @@ class VolumeApiTest(test.TestCase):
                 'size': 1,
                 'links': [
                     {
-                        'href': 'http://localhost/v2/fake/volumes/1',
+                        'href': 'http://localhost/v2/fakeproject/volumes/1',
                         'rel': 'self'
                     },
                     {
-                        'href': 'http://localhost/fake/volumes/1',
+                        'href': 'http://localhost/fakeproject/volumes/1',
                         'rel': 'bookmark'
                     }
                 ],
@@ -1124,11 +1126,11 @@ class VolumeApiTest(test.TestCase):
                 'size': 1,
                 'links': [
                     {
-                        'href': 'http://localhost/v2/fake/volumes/1',
+                        'href': 'http://localhost/v2/fakeproject/volumes/1',
                         'rel': 'self'
                     },
                     {
-                        'href': 'http://localhost/fake/volumes/1',
+                        'href': 'http://localhost/fakeproject/volumes/1',
                         'rel': 'bookmark'
                     }
                 ],

cinder/tests/test_volume.py
@@ -604,6 +604,28 @@ class VolumeTestCase(BaseVolumeTestCase):
         self.mox.UnsetStubs()
         self.volume.delete_volume(self.context, volume_id)
 
+    def test_get_volume_different_tenant(self):
+        """Test can't get volume of another tenant when viewable_admin_meta."""
+        volume = tests_utils.create_volume(self.context,
+                                           **self.volume_params)
+        volume_id = volume['id']
+        self.volume.create_volume(self.context, volume_id)
+
+        another_context = context.RequestContext('another_user_id',
+                                                 'another_project_id',
+                                                 is_admin=False)
+        self.assertNotEqual(another_context.project_id,
+                            self.context.project_id)
+
+        volume_api = cinder.volume.api.API()
+
+        self.assertRaises(exception.VolumeNotFound, volume_api.get,
+                          another_context, volume_id, viewable_admin_meta=True)
+        self.assertEqual(volume_id,
+                         volume_api.get(self.context, volume_id)['id'])
+
+        self.volume.delete_volume(self.context, volume_id)
+
     def test_delete_volume_in_error_extending(self):
         """Test volume can be deleted in error_extending stats."""
         # create a volume