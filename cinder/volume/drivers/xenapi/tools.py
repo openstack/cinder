cd0def136c73f3fc2059ce22f4d4a68b053e47d4
Bob Ball <bob.ball@citrix.com>
2014-09-22 16:29:00 +0100

    Remove XenAPI driver
    The XenAPI driver does not have known users and does not meet
    the minimum requirements for Cinder drivers.
    
    This patch removes the driver from the tree.
    
    Closes-bug: 1372836
    
    Change-Id: I2123b2d52180bbdffe90c458f115c67f50dfd5e9

--- cinder/volume/drivers/xenapi/tools.py
+++
@@ -1,7 +0,0 @@
-def _stripped_first_line_of(filename):
-    with open(filename, 'rb') as f:
-        return f.readline().strip()
-
-
-def get_this_vm_uuid():
-    return _stripped_first_line_of('/sys/hypervisor/uuid')
