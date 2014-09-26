40eff25fce9a350d1872b083503e4306242961de
Clinton Knight <cknight@netapp.com>
2014-09-26 12:07:44 -0400

    Deprecate / obsolete NetApp volume extra specs
    The NetApp Data ONTAP (Cluster-mode) NFS & iSCSI drivers for Juno support
    the Cinder pools feature, but the drivers are reporting two qualified
    extra specs that must be converted to unqualified extra specs in order to
    be used by the Cinder scheduler's capability filter. Furthermore, there
    are four extra specs that must be deprecated due to having the pools
    feature.  Warnings will be logged during volume creation if any of the
    obsolete or deprecated extra specs are seen in the volume type.
    
    Change-Id: I4dbd667610e481356304a12b8dae84cff61aa9d9
    Closes-bug: 1374630
    (cherry picked from commit 4cb4be4122a44dc99d6f29f065cdd32ae86273ce)

--- cinder/tests/volume/drivers/netapp/test_iscsi.py
+++ cinder/tests/volume/drivers/netapp/test_iscsi.py
@@ -82,7 +82,8 @@ class NetAppDirectISCSIDriverTestCase(test.TestCase):
     @mock.patch.object(iscsiDriver, 'create_lun', mock.Mock())
     @mock.patch.object(iscsiDriver, '_create_lun_handle', mock.Mock())
     @mock.patch.object(iscsiDriver, '_add_lun_to_table', mock.Mock())
-    @mock.patch.object(na_utils, 'get_volume_extra_specs',
+    @mock.patch.object(ntap_iscsi, 'LOG', mock.Mock())
+    @mock.patch.object(ntap_iscsi, 'get_volume_extra_specs',
                        mock.Mock(return_value=None))
     def test_create_volume(self):
         self.driver.create_volume({'name': 'lun1', 'size': 100,
@@ -90,12 +91,44 @@ class NetAppDirectISCSIDriverTestCase(test.TestCase):
                                    'host': 'hostname@backend#vol1'})
         self.driver.create_lun.assert_called_once_with(
             'vol1', 'lun1', 107374182400, mock.ANY, None)
+        self.assertEqual(0, ntap_iscsi.LOG.warn.call_count)
 
     def test_create_volume_no_pool_provided_by_scheduler(self):
         self.assertRaises(exception.InvalidHost, self.driver.create_volume,
                           {'name': 'lun1', 'size': 100,
                            'id': uuid.uuid4(),
                            'host': 'hostname@backend'})  # missing pool
+
+    @mock.patch.object(iscsiDriver, 'create_lun', mock.Mock())
+    @mock.patch.object(iscsiDriver, '_create_lun_handle', mock.Mock())
+    @mock.patch.object(iscsiDriver, '_add_lun_to_table', mock.Mock())
+    @mock.patch.object(na_utils, 'LOG', mock.Mock())
+    @mock.patch.object(ntap_iscsi, 'get_volume_extra_specs',
+                       mock.Mock(return_value={'netapp:raid_type': 'raid4'}))
+    def test_create_volume_obsolete_extra_spec(self):
+
+        self.driver.create_volume({'name': 'lun1', 'size': 100,
+                                   'id': uuid.uuid4(),
+                                   'host': 'hostname@backend#vol1'})
+        warn_msg = 'Extra spec netapp:raid_type is obsolete.  ' \
+                   'Use netapp_raid_type instead.'
+        na_utils.LOG.warn.assert_called_once_with(warn_msg)
+
+    @mock.patch.object(iscsiDriver, 'create_lun', mock.Mock())
+    @mock.patch.object(iscsiDriver, '_create_lun_handle', mock.Mock())
+    @mock.patch.object(iscsiDriver, '_add_lun_to_table', mock.Mock())
+    @mock.patch.object(na_utils, 'LOG', mock.Mock())
+    @mock.patch.object(ntap_iscsi, 'get_volume_extra_specs',
+                       mock.Mock(return_value={'netapp_thick_provisioned':
+                                               'true'}))
+    def test_create_volume_deprecated_extra_spec(self):
+
+        self.driver.create_volume({'name': 'lun1', 'size': 100,
+                                   'id': uuid.uuid4(),
+                                   'host': 'hostname@backend#vol1'})
+        warn_msg = 'Extra spec netapp_thick_provisioned is deprecated.  ' \
+                   'Use netapp_thin_provisioned instead.'
+        na_utils.LOG.warn.assert_called_once_with(warn_msg)
 
     def test_create_lun(self):
         expected_path = '/vol/%s/%s' % (self.fake_volume, self.fake_lun)
