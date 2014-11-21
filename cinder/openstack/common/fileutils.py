3efbbbde9fe7f5cf29126781ae2c3936edd47536
ChangBo Guo(gcb) <eric.guo@easystack.cn>
2014-11-21 17:48:18 +0800

    Don't use module excutils from oslo-incubator
    Remove module excutils and sync its only consumer module fileutils,
    which is using oslo.utils.
    
    Change-Id: Ib2c60be9febf5e15b198529b11c978d2e5b6df1f

--- cinder/openstack/common/fileutils.py
+++ cinder/openstack/common/fileutils.py
@@ -15,11 +15,11 @@
 
 import contextlib
 import errno
+import logging
 import os
 import tempfile
 
-from cinder.openstack.common import excutils
-from cinder.openstack.common import log as logging
+from oslo.utils import excutils
 
 LOG = logging.getLogger(__name__)
 
