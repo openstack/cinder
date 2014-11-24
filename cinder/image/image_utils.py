ab9a1ad7e1d52ab52f69f7705a5ff1a3a49da37c
Bharat Kumar Kobagana <bkobagan@redhat.com>
2014-11-24 14:08:26 +0530

    Create "image_conversion_dir" before creating temporary file
    In file "image_utils.py", "create_temporary_file" method is
    failing because, CONF.image_conversion_dir folder doesn't exist.
    
    This patch solves the issue by creating image_conversion_dir
    directory before calling "create_temporary_file" method.
    
    Change-Id: I546180e0521616fccdaf261d25fd4356d05f0274
    Closes-Bug: #1394578
    (cherry picked from commit c6d58672ed9666cbd9ad0d6454b064d8d05b43cc)

--- cinder/image/image_utils.py
+++ cinder/image/image_utils.py
@@ -372,6 +372,10 @@ def coalesce_vhd(vhd_path):
 
 
 def create_temporary_file(*args, **kwargs):
+    if (CONF.image_conversion_dir and not
+            os.path.exists(CONF.image_conversion_dir)):
+        os.makedirs(CONF.image_conversion_dir)
+
     fd, tmp = tempfile.mkstemp(dir=CONF.image_conversion_dir, *args, **kwargs)
     os.close(fd)
     return tmp
