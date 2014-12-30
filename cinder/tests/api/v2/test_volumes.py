openstack/cinder

commit 4aaf40ba1aab4d7c347b05750d0fe21f8d1bcc68

Fix issue with passing lists in filters
The Cinder db code already allows searching for a value in a list,
for example, id in ['id1', 'id2', 'id3']. The problem is when we
get the value at the API it comes as a string (e.g.
"['id1', 'id2', 'id3']", and therefore the filter fails. This was
already fixed for metadata filters, so just apply it to all filters.

We do a try/except because if it really is a string, then the
literal_eval will fail because we don't pass double quotes, such as
"'string'".  The current code therefore will now cover lists and
fall back to the original behavior for strings.

Change-Id: Idfd8298401a330bb99ddcd130268fabb6ef9d162
Closes-Bug: 1403875

cinder/api/v1/volumes.py
@@ -270,8 +270,11 @@ class VolumeController(wsgi.Controller):
         search_opts.pop('limit', None)
         search_opts.pop('offset', None)
 
-        if 'metadata' in search_opts:
-            search_opts['metadata'] = ast.literal_eval(search_opts['metadata'])
+        for k, v in search_opts.iteritems():
+            try:
+                search_opts[k] = ast.literal_eval(v)
+            except ValueError:
+                LOG.debug('Could not evaluate value %s, assuming string', v)
 
         context = req.environ['cinder.context']
         utils.remove_invalid_filter_options(context,

cinder/api/v2/volumes.py
@@ -230,8 +230,11 @@ class VolumeController(wsgi.Controller):
             filters['display_name'] = filters['name']
             del filters['name']
 
-        if 'metadata' in filters:
-            filters['metadata'] = ast.literal_eval(filters['metadata'])
+        for k, v in filters.iteritems():
+            try:
+                filters[k] = ast.literal_eval(v)
+            except ValueError:
+                LOG.debug('Could not evaluate value %s, assuming string', v)
 
         volumes = self.volume_api.get_all(context, marker, limit, sort_key,
                                           sort_dir, filters,

cinder/tests/api/v1/test_volumes.py
@@ -16,6 +16,7 @@
 import datetime
 
 from lxml import etree
+import mock
 from oslo.config import cfg
 import webob
 
@@ -760,6 +761,35 @@ class VolumeApiTest(test.TestCase):
         res = self.controller.index(req)
         self.assertIn('volumes', res)
         self.assertEqual(1, len(res['volumes']))
+
+    @mock.patch('cinder.utils.add_visible_admin_metadata')
+    @mock.patch('cinder.volume.api.API.get_all')
+    def test_get_volumes_filter_with_string(self, get_all, add_meta):
+        req = mock.MagicMock()
+        req.GET.copy.return_value = {'display_name': 'Volume-573108026'}
+        context = mock.Mock()
+        req.environ = {'cinder.context': context}
+        self.controller._items(req, mock.Mock)
+        get_all.assert_any_call(context, sort_dir='desc',
+                                viewable_admin_meta=True,
+                                sort_key='created_at',
+                                limit=None,
+                                filters={'display_name': 'Volume-573108026'},
+                                marker=None)
+
+    @mock.patch('cinder.utils.add_visible_admin_metadata')
+    @mock.patch('cinder.volume.api.API.get_all')
+    def test_get_volumes_filter_with_list(self, get_all, add_meta):
+        req = mock.MagicMock()
+        req.GET.copy.return_value = {'id': "['1', '2', '3']"}
+        context = mock.Mock()
+        req.environ = {'cinder.context': context}
+        self.controller._items(req, mock.Mock)
+        get_all.assert_any_call(context, sort_dir='desc',
+                                viewable_admin_meta=True,
+                                sort_key='created_at',
+                                limit=None, filters={'id': ['1', '2', '3']},
+                                marker=None)
 
 
 class VolumeSerializerTest(test.TestCase):

cinder/tests/api/v2/test_volumes.py
@@ -17,6 +17,7 @@
 import datetime
 
 from lxml import etree
+import mock
 from oslo.config import cfg
 import six
 import six.moves.urllib.parse as urlparse
@@ -1540,6 +1541,32 @@ class VolumeApiTest(test.TestCase):
     def test_create_malformed_entity(self):
         body = {'volume': 'string'}
         self._create_volume_bad_request(body=body)
+
+    @mock.patch('cinder.utils.add_visible_admin_metadata')
+    @mock.patch('cinder.volume.api.API.get_all')
+    def test_get_volumes_filter_with_string(self, get_all, add_meta):
+        req = mock.MagicMock()
+        context = mock.Mock()
+        req.environ = {'cinder.context': context}
+        req.params = {'display_name': 'Volume-573108026'}
+        self.controller._view_builder.detail_list = mock.Mock()
+        self.controller._get_volumes(req, True)
+        get_all.assert_any_call(context, None, None, 'created_at', 'desc',
+                                {'display_name': 'Volume-573108026'},
+                                viewable_admin_meta=True)
+
+    @mock.patch('cinder.utils.add_visible_admin_metadata')
+    @mock.patch('cinder.volume.api.API.get_all')
+    def test_get_volumes_filter_with_list(self, get_all, add_meta):
+        req = mock.MagicMock()
+        context = mock.Mock()
+        req.environ = {'cinder.context': context}
+        req.params = {'id': "['1', '2', '3']"}
+        self.controller._view_builder.detail_list = mock.Mock()
+        self.controller._get_volumes(req, True)
+        get_all.assert_any_call(context, None, None, 'created_at', 'desc',
+                                {'id': ['1', '2', '3']},
+                                viewable_admin_meta=True)
 
 
 class VolumeSerializerTest(test.TestCase):

commit f39eeebf6eca2745b83a79d2a0def57a126846e7

Fix handling of serialized data in filtering of volumes
Commit 4aaf40ba1aab4d7c347b05750d0fe21f8d1bcc68 has introduced a bug.
'ast.literal_eval(v)' throws exception 'SyntaxError' if 'v' is an expression
such as UUID '920da701-93c1-4178-9f1a-ef1c7a8a384d', 'd-', or 'd+'.
Catch the 'SyntaxError' exception in addition to 'ValueError' and
assume 'v' is a string. So the API can handle the request successfully
rather than returning a '500' error code.

Change-Id: Ib50cf3be41ae96ed1f9ef0463ced71443e59061c
Closes-Bug: #1406504

cinder/api/v1/volumes.py
@@ -273,7 +273,7 @@ class VolumeController(wsgi.Controller):
         for k, v in search_opts.iteritems():
             try:
                 search_opts[k] = ast.literal_eval(v)
-            except ValueError:
+            except (ValueError, SyntaxError):
                 LOG.debug('Could not evaluate value %s, assuming string', v)
 
         context = req.environ['cinder.context']

cinder/api/v2/volumes.py
@@ -233,7 +233,7 @@ class VolumeController(wsgi.Controller):
         for k, v in filters.iteritems():
             try:
                 filters[k] = ast.literal_eval(v)
-            except ValueError:
+            except (ValueError, SyntaxError):
                 LOG.debug('Could not evaluate value %s, assuming string', v)
 
         volumes = self.volume_api.get_all(context, marker, limit, sort_key,

cinder/tests/api/v1/test_volumes.py
@@ -762,34 +762,41 @@ class VolumeApiTest(test.TestCase):
         self.assertIn('volumes', res)
         self.assertEqual(1, len(res['volumes']))
 
-    @mock.patch('cinder.utils.add_visible_admin_metadata')
     @mock.patch('cinder.volume.api.API.get_all')
-    def test_get_volumes_filter_with_string(self, get_all, add_meta):
+    def test_get_volumes_filter_with_string(self, get_all):
         req = mock.MagicMock()
         req.GET.copy.return_value = {'display_name': 'Volume-573108026'}
         context = mock.Mock()
         req.environ = {'cinder.context': context}
         self.controller._items(req, mock.Mock)
-        get_all.assert_any_call(context, sort_dir='desc',
-                                viewable_admin_meta=True,
-                                sort_key='created_at',
-                                limit=None,
-                                filters={'display_name': 'Volume-573108026'},
-                                marker=None)
+        get_all.assert_called_once_with(
+            context, sort_dir='desc', viewable_admin_meta=True,
+            sort_key='created_at', limit=None,
+            filters={'display_name': 'Volume-573108026'}, marker=None)
 
-    @mock.patch('cinder.utils.add_visible_admin_metadata')
     @mock.patch('cinder.volume.api.API.get_all')
-    def test_get_volumes_filter_with_list(self, get_all, add_meta):
+    def test_get_volumes_filter_with_list(self, get_all):
         req = mock.MagicMock()
         req.GET.copy.return_value = {'id': "['1', '2', '3']"}
         context = mock.Mock()
         req.environ = {'cinder.context': context}
         self.controller._items(req, mock.Mock)
-        get_all.assert_any_call(context, sort_dir='desc',
-                                viewable_admin_meta=True,
-                                sort_key='created_at',
-                                limit=None, filters={'id': ['1', '2', '3']},
-                                marker=None)
+        get_all.assert_called_once_with(
+            context, sort_dir='desc', viewable_admin_meta=True,
+            sort_key='created_at', limit=None,
+            filters={'id': ['1', '2', '3']}, marker=None)
+
+    @mock.patch('cinder.volume.api.API.get_all')
+    def test_get_volumes_filter_with_expression(self, get_all):
+        req = mock.MagicMock()
+        req.GET.copy.return_value = {'id': "d+"}
+        context = mock.Mock()
+        req.environ = {'cinder.context': context}
+        self.controller._items(req, mock.Mock)
+        get_all.assert_called_once_with(
+            context, sort_dir='desc', viewable_admin_meta=True,
+            sort_key='created_at', limit=None, filters={'id': 'd+'},
+            marker=None)
 
 
 class VolumeSerializerTest(test.TestCase):

cinder/tests/api/v2/test_volumes.py
@@ -1542,31 +1542,41 @@ class VolumeApiTest(test.TestCase):
         body = {'volume': 'string'}
         self._create_volume_bad_request(body=body)
 
-    @mock.patch('cinder.utils.add_visible_admin_metadata')
     @mock.patch('cinder.volume.api.API.get_all')
-    def test_get_volumes_filter_with_string(self, get_all, add_meta):
+    def test_get_volumes_filter_with_string(self, get_all):
         req = mock.MagicMock()
         context = mock.Mock()
         req.environ = {'cinder.context': context}
         req.params = {'display_name': 'Volume-573108026'}
         self.controller._view_builder.detail_list = mock.Mock()
         self.controller._get_volumes(req, True)
-        get_all.assert_any_call(context, None, None, 'created_at', 'desc',
-                                {'display_name': 'Volume-573108026'},
-                                viewable_admin_meta=True)
+        get_all.assert_called_once_with(
+            context, None, None, 'created_at', 'desc',
+            {'display_name': 'Volume-573108026'}, viewable_admin_meta=True)
 
-    @mock.patch('cinder.utils.add_visible_admin_metadata')
     @mock.patch('cinder.volume.api.API.get_all')
-    def test_get_volumes_filter_with_list(self, get_all, add_meta):
+    def test_get_volumes_filter_with_list(self, get_all):
         req = mock.MagicMock()
         context = mock.Mock()
         req.environ = {'cinder.context': context}
         req.params = {'id': "['1', '2', '3']"}
         self.controller._view_builder.detail_list = mock.Mock()
         self.controller._get_volumes(req, True)
-        get_all.assert_any_call(context, None, None, 'created_at', 'desc',
-                                {'id': ['1', '2', '3']},
-                                viewable_admin_meta=True)
+        get_all.assert_called_once_with(
+            context, None, None, 'created_at', 'desc',
+            {'id': ['1', '2', '3']}, viewable_admin_meta=True)
+
+    @mock.patch('cinder.volume.api.API.get_all')
+    def test_get_volumes_filter_with_expression(self, get_all):
+        req = mock.MagicMock()
+        context = mock.Mock()
+        req.environ = {'cinder.context': context}
+        req.params = {'name': "d-"}
+        self.controller._view_builder.detail_list = mock.Mock()
+        self.controller._get_volumes(req, True)
+        get_all.assert_called_once_with(
+            context, None, None, 'created_at', 'desc',
+            {'display_name': 'd-'}, viewable_admin_meta=True)
 
 
 class VolumeSerializerTest(test.TestCase):