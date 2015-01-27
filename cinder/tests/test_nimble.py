7f88689a2038b50d0f7b7f01c5086e648f15e81a
Jay Wang <jwang@nimblestorage.com>
2015-01-27 12:02:59 -0800

    Fixes attribute content checking
    Use proper way to check the volume attribute contents in display_name
    and display_description. If they are not empty, translate them to
    volume backend description. Modify other places where return empty
    string makes sense than None as well as the return value checking.
    
    Closes-Bug: 1414247
    Change-Id: I87b8d09baf75b227b479f8a79ace90a85cf84177

--- cinder/tests/test_nimble.py
+++ cinder/tests/test_nimble.py
@@ -215,7 +215,9 @@ class NimbleDriverVolumeTestCase(NimbleDriverBaseTestCase):
             'provider_location': '172.18.108.21:3260 iqn.test 0',
             'provider_auth': None},
             self.driver.create_volume({'name': 'testvolume',
-                                       'size': 1}))
+                                       'size': 1,
+                                       'display_name': '',
+                                       'display_description': ''}))
         self.mock_client_service.service.createVol.assert_called_once_with(
             request={
                 'attr': {'snap-quota': 1073741824, 'warn-level': 858993459,
@@ -236,7 +238,9 @@ class NimbleDriverVolumeTestCase(NimbleDriverBaseTestCase):
             exception.VolumeBackendAPIException,
             self.driver.create_volume,
             {'name': 'testvolume',
-             'size': 1})
+             'size': 1,
+             'display_name': '',
+             'display_description': ''})
 
     @mock.patch(NIMBLE_URLLIB2)
     @mock.patch(NIMBLE_CLIENT)
@@ -352,7 +356,9 @@ class NimbleDriverSnapshotTestCase(NimbleDriverBaseTestCase):
             FAKE_GENERIC_POSITIVE_RESPONSE
         self.driver.create_snapshot(
             {'volume_name': 'testvolume',
-             'name': 'testvolume-snap1'})
+             'name': 'testvolume-snap1',
+             'display_name': '',
+             'display_description': ''})
         self.mock_client_service.service.snapVol.assert_called_once_with(
             request={'vol': 'testvolume',
                      'snapAttr': {'name': 'testvolume-snap1',
