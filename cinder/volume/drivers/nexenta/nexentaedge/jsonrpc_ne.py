3d7d571180a4cee82c3cc732f2af5fe8cfef95d4
Mikhail Khodos <mikhail.khodos@nexenta.com>
2015-03-05 18:28:44 +0300

    Fix comments

--- cinder/volume/drivers/nexenta/nexentaedge/jsonrpc_ne.py
+++ cinder/volume/drivers/nexenta/nexentaedge/jsonrpc_ne.py
@@ -13,7 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 """
-:mod:`nexenta.jsonrpc` -- Nexenta-specific JSON RPC client
+:mod:`nexentaedge.jsonrpc` -- NexentaEdge-specific JSON RPC client
 =====================================================================
 
 .. automodule:: nexentaedge.jsonrpc
