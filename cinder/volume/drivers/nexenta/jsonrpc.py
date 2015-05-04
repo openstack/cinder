2212f420fee3920e3ccf2b168ef606a1f41a49ef
Mikhail Khodos <mikhail.khodos@nexenta.com>
2015-05-04 06:38:52 -0600

    Fix import

--- cinder/volume/drivers/nexenta/jsonrpc.py
+++ cinder/volume/drivers/nexenta/jsonrpc.py
@@ -25,7 +25,7 @@ import urllib2
 from oslo_serialization import jsonutils
 
 from cinder.i18n import _, _LE, _LI
-from cinder.openstack.common import log as logging
+from oslo_log import log as logging
 from cinder.volume.drivers import nexenta
 
 LOG = logging.getLogger(__name__)
