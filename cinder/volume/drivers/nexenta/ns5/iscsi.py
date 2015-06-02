c670f111a881ec293c63143ad8d515d2cac2e20e
Alexey Khodos <alexey.khodos@nexenta.com>
2015-06-02 19:10:32 +0300

    clone volume, removed unused code

--- cinder/volume/drivers/nexenta/ns5/iscsi.py
+++ cinder/volume/drivers/nexenta/ns5/iscsi.py
@@ -23,9 +23,8 @@
 from oslo_log import log as logging
 from oslo_utils import units
 
-from cinder import context, db
 from cinder import exception
-from cinder.i18n import _, _LE, _LI, _LW
+from cinder.i18n import _, _LE, _LW
 from cinder.volume import driver
 from cinder.volume.drivers import nexenta
 from cinder.volume.drivers.nexenta.ns5 import jsonrpc
@@ -247,10 +246,6 @@ class NexentaISCSIDriver(driver.ISCSIDriver):  # pylint: disable=R0921
               }
         self.nef(url, {'name': volume['name']})
 
-    def _get_snapshot_volume(self, snapshot):
-        ctxt = context.get_admin_context()
-        return db.volume_get(ctxt, snapshot['volume_id'])
-
     def create_cloned_volume(self, volume, src_vref):
         """Creates a clone of the specified volume.
 
@@ -278,110 +273,13 @@ class NexentaISCSIDriver(driver.ISCSIDriver):  # pylint: disable=R0921
                                 '%(volume_name)s@%(name)s'), snapshot)
             raise
 
-    def _get_zfs_send_recv_cmd(self, src, dst):
-        """Returns rrmgr command for source and destination."""
-        return utils.get_rrmgr_cmd(src, dst,
-                                   compression=self.rrmgr_compression,
-                                   tcp_buf_size=self.rrmgr_tcp_buf_size,
-                                   connections=self.rrmgr_connections)
-
     @staticmethod
-    def get_nms_for_url(url):
+    def get_nef_for_url(url):
         """Returns initialized nms object for url."""
-        auto, scheme, user, password, host, port, path =\
-            utils.parse_nms_url(url)
-        return jsonrpc.NexentaJSONProxy(scheme, host, port, path, user,
+        auto, scheme, user, password, host, port =\
+            utils.parse_nef_url(url)
+        return jsonrpc.NexentaJSONProxy(scheme, host, port, user,
                                         password, auto=auto)
-
-    def migrate_volume(self, ctxt, volume, host):
-        """Migrate if volume and host are managed by Nexenta appliance.
-
-        :param ctxt: context
-        :param volume: a dictionary describing the volume to migrate
-        :param host: a dictionary describing the host to migrate to
-        """
-        LOG.debug('Enter: migrate_volume: id=%(id)s, host=%(host)s' %
-                  {'id': volume['id'], 'host': host})
-        false_ret = (False, None)
-
-        if volume['status'] not in ('available', 'retyping'):
-            return false_ret
-
-        if 'capabilities' not in host:
-            return false_ret
-
-        capabilities = host['capabilities']
-
-        if 'location_info' not in capabilities or \
-                'iscsi_target_portal_port' not in capabilities or \
-                'nms_url' not in capabilities:
-            return false_ret
-
-        nms_url = capabilities['nms_url']
-        dst_parts = capabilities['location_info'].split(':')
-
-        if capabilities.get('vendor_name') != 'Nexenta' or \
-                dst_parts[0] != self.__class__.__name__ or \
-                capabilities['free_capacity_gb'] < volume['size']:
-            return false_ret
-
-        dst_host, dst_volume = dst_parts[1:]
-
-        ssh_bound = False
-        ssh_bindings = self.nms.appliance.ssh_list_bindings()
-        for bind in ssh_bindings:
-            if dst_host.startswith(ssh_bindings[bind][3]):
-                ssh_bound = True
-                break
-        if not ssh_bound:
-            LOG.warning(_LW("Remote NexentaStor appliance at %s should be "
-                            "SSH-bound."), dst_host)
-
-        # Create temporary snapshot of volume on NexentaStor Appliance.
-        snapshot = {
-            'volume_name': volume['name'],
-            'name': utils.get_migrate_snapshot_name(volume)
-        }
-        self.create_snapshot(snapshot)
-
-        src = '%(volume)s/%(zvol)s@%(snapshot)s' % {
-            'volume': self.volume,
-            'zvol': volume['name'],
-            'snapshot': snapshot['name']
-        }
-        dst = ':'.join([dst_host, dst_volume])
-
-        try:
-            self.nms.appliance.execute(self._get_zfs_send_recv_cmd(src, dst))
-        except nexenta.NexentaException as exc:
-            LOG.warning(_LW("Cannot send source snapshot %(src)s to "
-                            "destination %(dst)s. Reason: %(exc)s"),
-                        {'src': src, 'dst': dst, 'exc': exc})
-            return false_ret
-        finally:
-            try:
-                self.delete_snapshot(snapshot)
-            except nexenta.NexentaException as exc:
-                LOG.warning(_LW("Cannot delete temporary source snapshot "
-                                "%(src)s on NexentaStor Appliance: %(exc)s"),
-                            {'src': src, 'exc': exc})
-        try:
-            self.delete_volume(volume)
-        except nexenta.NexentaException as exc:
-            LOG.warning(_LW("Cannot delete source volume %(volume)s on "
-                            "NexentaStor Appliance: %(exc)s"),
-                        {'volume': volume['name'], 'exc': exc})
-
-        dst_nms = self.get_nms_for_url(nms_url)
-        dst_snapshot = '%s/%s@%s' % (dst_volume, volume['name'],
-                                     snapshot['name'])
-        try:
-            dst_nms.snapshot.destroy(dst_snapshot, '')
-        except nexenta.NexentaException as exc:
-            LOG.warning(_LW("Cannot delete temporary destination snapshot "
-                            "%(dst)s on NexentaStor Appliance: %(exc)s"),
-                        {'dst': dst_snapshot, 'exc': exc})
-        return True, None
 
     def local_path(self, volume):
         """Return local path to existing local volume.
@@ -427,20 +325,6 @@ class NexentaISCSIDriver(driver.ISCSIDriver):  # pylint: disable=R0921
             return False
         return target_member in members
 
-    def _lu_exists(self, zvol_name):
-        """Check if LU exists on appliance.
-
-        :param zvol_name: Zvol name
-        :raises: NexentaException if zvol not exists
-        :return: True if LU exists, else False
-        """
-        try:
-            return bool(self.nms.scsidisk.lu_exists(zvol_name))
-        except nexenta.NexentaException as exc:
-            if 'does not exist' not in exc.args[0]:
-                raise
-            return False
-
     def _is_lu_shared(self, zvol_name):
         """Check if LU exists on appliance and shared.
 
@@ -472,30 +356,8 @@ class NexentaISCSIDriver(driver.ISCSIDriver):  # pylint: disable=R0921
         else:
             return self.nef(url)['data'][0]['viewCount']
 
-    def _is_volume_exported(self, volume):
-        """Check if volume exported.
-
-        :param volume: volume object
-        :return: True if volume exported, else False
-        """
-        zvol_name = self._get_zvol_name(volume['name'])
-        target_name = self._get_target_name(volume['name'])
-        target_group_name = self._get_target_group_name(volume['name'])
-        return (self._target_exists(target_name) and
-                self._target_group_exists(target_group_name) and
-                self._target_member_in_target_group(target_group_name,
-                                                    target_name) and
-                self._lu_exists(zvol_name) and
-                self._is_lu_shared(zvol_name))
-
     def _get_provider_location(self, volume):
         """Returns volume iscsiadm-formatted provider location string."""
-        LOG.warning('%(host)s:%(port)s,1 %(name)s' % {
-            'host': self.nef_host,
-            'port': self.configuration.nexenta_iscsi_target_portal_port,
-            'name': self._get_target_name(volume['name']),
-            'lun': self._get_lun(volume['name'])
-        })
         return '%(host)s:%(port)s,1 %(name)s' % {
             'host': self.nef_host,
             'port': self.configuration.nexenta_iscsi_target_portal_port,
