e32046d33023e296e1b976bc1312ee7aea4e8e9b
Alexey Khodos <alexey.khodos@nexenta.com>
2015-06-12 12:01:24 +0300

    snapshot volume_name without db call

--- cinder/volume/drivers/nexenta/ns5/iscsi.py
+++ cinder/volume/drivers/nexenta/ns5/iscsi.py
@@ -209,18 +209,17 @@ class NexentaISCSIDriver(driver.ISCSIDriver):  # pylint: disable=R0921
 
         :param snapshot: snapshot reference
         """
-        snapshot_vol = self._get_snapshot_volume(snapshot)
         LOG.info(_LI('Creating snapshot %(snap)s of volume %(vol)s') % {
             'snap': snapshot['name'],
-            'vol': snapshot_vol['name']
+            'vol': snapshot.volume_name
         })
-        zvol_name = self._get_zvol_name(snapshot_vol['name'])
+        zvol_name = self._get_zvol_name(snapshot.volume_name)
         pool, group, volume = zvol_name.split('/')
         url = 'storage/pools/%(pool)s/datasetGroups/%(group)s/' \
               'volumes/%(volume)s/snapshots' % {
                   'pool': pool,
                   'group': group,
-                  'volume': snapshot_vol['name']
+                  'volume': snapshot.volume_name
               }
         self.nef(url, {'name': snapshot['name']})
 
@@ -230,8 +229,7 @@ class NexentaISCSIDriver(driver.ISCSIDriver):  # pylint: disable=R0921
         :param snapshot: snapshot reference
         """
         LOG.info(_LI('Deleting snapshot: %s') % snapshot['name'])
-        snapshot_vol = self._get_snapshot_volume(snapshot)
-        zvol_name = self._get_zvol_name(snapshot_vol['name'])
+        zvol_name = self._get_zvol_name(snapshot.volume_name)
         pool, group, volume = zvol_name.split('/')
         url = ('storage/pools/%(pool)s/datasetGroups/%(group)s/'
                'volumes/%(volume)s/snapshots/%(snapshot)s') % {
@@ -255,8 +253,7 @@ class NexentaISCSIDriver(driver.ISCSIDriver):  # pylint: disable=R0921
         :param snapshot: reference of source snapshot
         """
         LOG.info(_LI('Creating volume from snapshot: %s') % snapshot['name'])
-        snapshot_vol = self._get_snapshot_volume(snapshot)
-        zvol_name = self._get_zvol_name(snapshot_vol['name'])
+        zvol_name = self._get_zvol_name(snapshot.volume_name)
         pool, group, snapshot_vol = zvol_name.split('/')
         url = ('storage/pools/%(pool)s/datasetGroups/%(group)s/'
                'volumes/%(volume)s/snapshots/%(snapshot)s/clone') % {
