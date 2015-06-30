c67d8b2d2ddbf62640443c29b2879019411e7564
Victor Stinner <vstinner@redhat.com>
2015-06-30 16:06:28 +0200

    Port image/glance.py to Python 3
    * Fix usage of six.reraise(): new_exc is the exception value, not the
      exception type
    * Replace __builtin__ with six.moves.builtins.
    * test_extracting_v2_boot_properties(): set config.glance_num_retries to
      fix the test on Python 3 (comparison between mock and int now raises
      a TypeError on Python 3).
    * TestGlanceSerializer: use dictionaries with only one key to have a
      reliable output even if the hash is randomized (hash randomization is
      now enabled by default in Python 3).
    * tox.ini: add cinder.tests.unit.image.test_glance to Python 3.4.
    
    Blueprint cinder-python3
    Change-Id: I33cd02e1b0666d7b5999b2fdaf469dc59fff1866

--- cinder/image/glance.py
+++ cinder/image/glance.py
@@ -482,14 +482,14 @@ def _reraise_translated_image_exception(image_id):
     """Transform the exception for the image but keep its traceback intact."""
     _exc_type, exc_value, exc_trace = sys.exc_info()
     new_exc = _translate_image_exception(image_id, exc_value)
-    six.reraise(new_exc, None, exc_trace)
+    six.reraise(type(new_exc), new_exc, exc_trace)
 
 
 def _reraise_translated_exception():
     """Transform the exception but keep its traceback intact."""
     _exc_type, exc_value, exc_trace = sys.exc_info()
     new_exc = _translate_plain_exception(exc_value)
-    six.reraise(new_exc, None, exc_trace)
+    six.reraise(type(new_exc), new_exc, exc_trace)
 
 
 def _translate_image_exception(image_id, exc_value):
