a300ebf0db8b02c13bdf0ad65bc38f57d2bc976d
Tom Swanson <tom_swanson@dell.com>
2015-07-08 10:46:09 -0500

    Dell SC: Add support for ManageableVD
    Added support for the ManageableVD volume driver class. This also
    meant the driver changed from inheriting the volumedriver class to
    inheriting the assorted *VD classes.
    
    A few things about our ManageableVD support. We only allow management
    of a volume if it is sized in multiples of 1GB. We only allow
    management of volumes that are not currently mapped to a server on the
    Storage Center. id-types of source-id (DeviceId on the Storage Center)
    and source-name (Volume Name on the Storage Center) are supported.
    
    Change-Id: Iecc93ce1654111e1292cfc1931d1eda4abd08595

--- cinder/volume/drivers/dell/dell_storagecenter_common.py
+++ cinder/volume/drivers/dell/dell_storagecenter_common.py
@@ -48,7 +48,9 @@ CONF = cfg.CONF
 CONF.register_opts(common_opts)
 
 
-class DellCommonDriver(driver.VolumeDriver):
+class DellCommonDriver(driver.ConsistencyGroupVD, driver.ManageableVD,
+                       driver.ExtendVD, driver.CloneableVD, driver.SnapshotVD,
+                       driver.BaseVD):
 
     def __init__(self, *args, **kwargs):
         super(DellCommonDriver, self).__init__(*args, **kwargs)
@@ -374,6 +376,7 @@ class DellCommonDriver(driver.VolumeDriver):
         # The world was horrible to us so we should error and leave.
         LOG.error(_LE('Unable to rename the logical volume for volume: %s'),
                   original_volume_name)
+
         return {'_name_id': new_volume['_name_id'] or new_volume['id']}
 
     def create_consistencygroup(self, context, group):
@@ -530,3 +533,77 @@ class DellCommonDriver(driver.VolumeDriver):
             model_update = {'status': 'deleted'}
 
             return model_update, snapshots
+
+    def manage_existing(self, volume, existing_ref):
+        """Brings an existing backend storage object under Cinder management.
+
+        existing_ref is passed straight through from the API request's
+        manage_existing_ref value, and it is up to the driver how this should
+        be interpreted.  It should be sufficient to identify a storage object
+        that the driver should somehow associate with the newly-created cinder
+        volume structure.
+
+        There are two ways to do this:
+
+        1. Rename the backend storage object so that it matches the,
+           volume['name'] which is how drivers traditionally map between a
+           cinder volume and the associated backend storage object.
+
+        2. Place some metadata on the volume, or somewhere in the backend, that
+           allows other driver requests (e.g. delete, clone, attach, detach...)
+           to locate the backend storage object when required.
+
+        If the existing_ref doesn't make sense, or doesn't refer to an existing
+        backend storage object, raise a ManageExistingInvalidReference
+        exception.
+
+        The volume may have a volume_type, and the driver can inspect that and
+        compare against the properties of the referenced backend storage
+        object.  If they are incompatible, raise a
+        ManageExistingVolumeTypeMismatch, specifying a reason for the failure.
+
+        :param volume:       Cinder volume to manage
+        :param existing_ref: Driver-specific information used to identify a
+        volume
+        """
+        if existing_ref.get('source-name') or existing_ref.get('source-id'):
+            with self._client.open_connection() as api:
+                api.manage_existing(volume['id'], existing_ref)
+        else:
+            raise exception.ManageExistingInvalidReference(
+                _('Must specify source-name or source-id. (%s)') %
+                existing_ref)
+
+    def manage_existing_get_size(self, volume, existing_ref):
+        """Return size of volume to be managed by manage_existing.
+
+        When calculating the size, round up to the next GB.
+
+        :param volume:       Cinder volume to manage
+        :param existing_ref: Driver-specific information used to identify a
+        volume
+        """
+        if existing_ref.get('source-name') or existing_ref.get('source-id'):
+            with self._client.open_connection() as api:
+                return api.get_unmanaged_volume_size(existing_ref)
+        else:
+            raise exception.ManageExistingInvalidReference(
+                _('Must specify source-name or source-id. (%s)') %
+                existing_ref)
+
+    def unmanage(self, volume):
+        """Removes the specified volume from Cinder management.
+
+        Does not delete the underlying backend storage object.
+
+        For most drivers, this will not need to do anything.  However, some
+        drivers might use this call as an opportunity to clean up any
+        Cinder-specific configuration that they have associated with the
+        backend storage object.
+
+        :param volume: Cinder volume to unmanage
+        """
+        with self._client.open_connection() as api:
+            scvolume = api.find_volume(volume['id'])
+            if scvolume:
+                api.unmanage(scvolume)
