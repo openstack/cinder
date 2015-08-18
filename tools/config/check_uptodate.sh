683dd65d418a4910b856b99c9378bb24a19c439c
Eric Harney <eharney@redhat.com>
2015-08-18 17:09:50 -0400

    Test whether sample config generation works
    Adds --checkonly to tools/config/check_uptodate.sh.
    
    This allows verifying that a configuration file was
    generated.
    
    We have had a number of issues introduced which
    have caused config file generation to fail since we
    removed the sample config file check in the gate.
    This does not add back checks to ensure that the file
    has been updated, it only checks to ensure that it is
    still possible to update the sample file.
    
    Change-Id: I2e0e376fbdec5a1bb584c6739231649e1d0f5d8e

--- tools/config/check_uptodate.sh
+++ tools/config/check_uptodate.sh
@@ -1,28 +1,44 @@
 #!/usr/bin/env bash
 
+CHECKONLY=0
+if [ "$1" == "--checkonly" ]; then
+    CHECKONLY=1
+fi
+
 PROJECT_NAME=${PROJECT_NAME:-cinder}
 CFGFILE_NAME=${PROJECT_NAME}.conf.sample
 
-if [ -e etc/${PROJECT_NAME}/${CFGFILE_NAME} ]; then
-    CFGFILE=etc/${PROJECT_NAME}/${CFGFILE_NAME}
-elif [ -e etc/${CFGFILE_NAME} ]; then
-    CFGFILE=etc/${CFGFILE_NAME}
-else
-    echo "${0##*/}: can not find config file"
-    exit 1
-fi
 
 TEMPDIR=`mktemp -d /tmp/${PROJECT_NAME}.XXXXXX`
 trap "rm -rf $TEMPDIR" EXIT
 
 tools/config/generate_sample.sh -b ./ -p ${PROJECT_NAME} -o ${TEMPDIR}
 
-if ! diff -u ${TEMPDIR}/${CFGFILE_NAME} ${CFGFILE}
-then
-   echo "${0##*/}: ${PROJECT_NAME}.conf.sample is not up to date."
-   echo "${0##*/}: Please run ${0%%${0##*/}}generate_sample.sh from within a VENV."
-   echo "  \'source .venv/bin/activate; generate_sample.sh\'"
-   echo "OR simply run tox genconfig"
-   echo "  \'tox -egenconfig\'"
-   exit 1
+# generate_sample.sh may return 0 even when it fails.
+
+if [ $CHECKONLY -eq 1 ]; then
+    # Check whether something was generated.
+    if [ ! -s ${TEMPDIR}/${CFGFILE_NAME} ]; then
+        echo "Failed to generate ${CFGFILE_NAME}."
+        exit 1
+    fi
+else
+    if [ -e etc/${PROJECT_NAME}/${CFGFILE_NAME} ]; then
+        CFGFILE=etc/${PROJECT_NAME}/${CFGFILE_NAME}
+    elif [ -e etc/${CFGFILE_NAME} ]; then
+        CFGFILE=etc/${CFGFILE_NAME}
+    else
+        echo "${0##*/}: can not find config file"
+        exit 1
+    fi
+
+    if ! diff -u ${TEMPDIR}/${CFGFILE_NAME} ${CFGFILE}
+    then
+        echo "${0##*/}: ${PROJECT_NAME}.conf.sample is not up to date."
+        echo "${0##*/}: Please run ${0%%${0##*/}}generate_sample.sh from within a VENV."
+        echo "  \'source .venv/bin/activate; generate_sample.sh\'"
+        echo "OR simply run tox genconfig"
+        echo "  \'tox -egenconfig\'"
+        exit 1
+    fi
 fi
