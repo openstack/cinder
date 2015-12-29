5ad04602e575fe420cae3205df9ba0f1a2dfc508
Alexey <alexey.khodos@nexenta.com>
2015-12-29 03:07:31 +0300

    fixed delete origin

--- cinder/volume/drivers/nexenta/ns5/nfs.py
+++ cinder/volume/drivers/nexenta/ns5/nfs.py
@@ -272,7 +272,7 @@ class NexentaNfsDriver(nfs.NfsDriver):  # pylint: disable=R0921
                 snap_url = ('storage/pools/%(pool)s/'
                             'filesystems/%(fs)s/snapshots/%(snap)s') % {
                     'pool': pool,
-                    'fs': fs,
+                    'fs': '%2F'.join([fs, volume['name']]),
                     'snap': origin.split('@')[-1]
                 }
                 nef(snap_url, method='DELETE')
