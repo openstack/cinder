04d146cae659cf45dfb9d6d16a1bb2d1e293e659
Alexey <alexey.khodos@nexenta.com>
2016-01-12 12:26:29 +0300

    update copyrights

--- cinder/volume/drivers/nexenta/iscsi.py
+++ cinder/volume/drivers/nexenta/iscsi.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Nexenta Systems, Inc. All Rights Reserved.
+# Copyright 2016 Nexenta Systems, Inc. All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
--- cinder/volume/drivers/nexenta/nfs.py
+++ cinder/volume/drivers/nexenta/nfs.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Nexenta Systems, Inc.
+# Copyright 2016 Nexenta Systems, Inc.
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
