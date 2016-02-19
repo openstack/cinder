492b350a3fec957263430a1e67457a7386254c61
Tom Barron <tpb@dyncloud.net>
2016-02-19 18:19:14 -0500

    Fix retype failure when original has no volume type
    cinder.objects.volume.Volume.finish_volume_migration()
    should skip the volume_type_id field when swapping fields
    between "source" and "dest" volumes.  The "dest" volume
    has already been created with appropriate volume_type_id
    and the "source" may have not have a volume type.
    
    This commit also changes a LOG.error() to a LOG.exception()
    in where finish_volume_migration is called in order to
    show the exception traceback in its calling context.
    
    Change-Id: I2caf4d3f4aa088d099548e6e88d1776b4cc5810c
    Closes-bug: #1547546

--- cinder/objects/volume.py
+++ cinder/objects/volume.py
@@ -394,8 +394,8 @@ class Volume(base.CinderPersistentObject, base.CinderObject,
         # We swap fields between source (i.e. self) and destination at the
         # end of migration because we want to keep the original volume id
         # in the DB but now pointing to the migrated volume.
-        skip = ({'id', 'provider_location', 'glance_metadata'} |
-                set(self.obj_extra_fields))
+        skip = ({'id', 'provider_location', 'glance_metadata',
+                 'volume_type_id'} | set(self.obj_extra_fields))
         for key in set(dest_volume.fields.keys()) - skip:
             # Only swap attributes that are already set.  We do not want to
             # unexpectedly trigger a lazy-load.
