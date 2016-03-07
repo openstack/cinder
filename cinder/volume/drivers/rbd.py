fba1af2bb1382af029e0340512f30b1ba10d389b
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2016-03-07 16:25:09 +0100

    rbd: Change capacity calculation from integer to float
    The current capacity calculation in the rbd driver use old
    c-style integer calculation while e.g. the scheduler
    _weigh_object function uses float, therefore calculate
    capacity information in float with 2 digits precision for
    Gb sizes.
    
    Closes-Bug: #1508340
    
    Change-Id: I1a731c7606a7dee0295b1604b582447d2da6e750
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- cinder/volume/drivers/rbd.py
+++ cinder/volume/drivers/rbd.py
@@ -404,11 +404,12 @@ class RBDDriver(driver.TransferVD, driver.ExtendVD,
                     pool_stats = [pool for pool in outbuf['pools'] if
                                   pool['name'] ==
                                   self.configuration.rbd_pool][0]['stats']
-                    stats['free_capacity_gb'] = (
-                        pool_stats['max_avail'] // units.Gi)
-                    used_capacity_gb = pool_stats['bytes_used'] // units.Gi
-                    stats['total_capacity_gb'] = (stats['free_capacity_gb']
-                                                  + used_capacity_gb)
+                    stats['free_capacity_gb'] = round((float(
+                        pool_stats['max_avail']) / units.Gi), 2)
+                    used_capacity_gb = float(
+                        pool_stats['bytes_used']) / units.Gi
+                    stats['total_capacity_gb'] = round(
+                        (stats['free_capacity_gb'] + used_capacity_gb), 2)
         except self.rados.Error:
             # just log and return unknown capacities
             LOG.exception(_LE('error refreshing volume stats'))
