f257d77aabb92b2992ce246dd95dc9929f6800af
Michael Price <michael.price@netapp.com>
2016-03-10 11:17:18 -0600

    NetApp E-Series: Volumes not added to consisgroup
    Volumes were not being added to a consistency group upon creation when the
    consistencygroup_id is provided during the volume creation.
    
    This patch adds functionality to relevant methods in library.py to add the newly
    created volume to a consistency group when applicable.
    
    Closes-Bug: #1555933
    Change-Id: I6d09b81571ce7b5c07d155cb4a70079fe35ed945

--- cinder/volume/drivers/netapp/eseries/library.py
+++ cinder/volume/drivers/netapp/eseries/library.py
@@ -499,6 +499,11 @@ class NetAppESeriesLibrary(object):
         if storage_pool:
             return storage_pool.get('label')
 
+    def _add_volume_to_consistencygroup(self, volume):
+        if volume.get('consistencygroup_id'):
+            es_cg = self._get_consistencygroup(volume['consistencygroup'])
+            self._update_consistency_group_members(es_cg, [volume], [])
+
     def create_volume(self, volume):
         """Creates a volume."""
 
@@ -520,6 +525,8 @@ class NetAppESeriesLibrary(object):
         size_gb = int(volume['size'])
         self._create_volume(eseries_pool_label, eseries_volume_label, size_gb,
                             extra_specs)
+
+        self._add_volume_to_consistencygroup(volume)
 
     def _create_volume(self, eseries_pool_label, eseries_volume_label,
                        size_gb, extra_specs=None):
@@ -665,6 +672,8 @@ class NetAppESeriesLibrary(object):
         cinder_utils.synchronized(snapshot['id'])(
             self._create_volume_from_snapshot)(volume, es_snapshot)
 
+        self._add_volume_to_consistencygroup(volume)
+
     def _copy_volume_high_priority_readonly(self, src_vol, dst_vol):
         """Copies src volume to dest volume."""
         LOG.info(_LI("Copying src vol %(src)s to dest vol %(dst)s."),
@@ -711,6 +720,7 @@ class NetAppESeriesLibrary(object):
 
         try:
             self._create_volume_from_snapshot(volume, es_snapshot)
+            self._add_volume_to_consistencygroup(volume)
         finally:
             try:
                 self._client.delete_snapshot_group(es_snapshot['pitGroupRef'])
