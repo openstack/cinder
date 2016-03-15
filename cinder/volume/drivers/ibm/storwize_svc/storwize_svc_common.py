7f17efc1286b94b3548fed82e240876f7af613f1
Shyama Venugopal <shyvenug@in.ibm.com>
2016-03-15 06:46:13 -0400

    Storwize: Set multiattach property per pool
    The cinder-scheduler capabilities filter checks the capability
    of each pool and if multiattach is not set in the pool
    properties creation will fail even if multihostmap is enabled on the
    config. Setting the multiattach property based on config in each
    pool stats.
    
    Change-Id: I858a3d9e1d397e8468e6f1b2e33d999eef28d5b6
    Closes-Bug: #1557420

--- cinder/volume/drivers/ibm/storwize_svc/storwize_svc_common.py
+++ cinder/volume/drivers/ibm/storwize_svc/storwize_svc_common.py
@@ -3093,9 +3093,6 @@ class StorwizeSVCCommonDriver(san.SanDriver,
         data['storage_protocol'] = self.protocol
         data['pools'] = []
 
-        data['multiattach'] = (self.configuration.
-                               storwize_svc_multihostmap_enabled)
-
         backend_name = self.configuration.safe_get('volume_backend_name')
         data['volume_backend_name'] = (backend_name or
                                        self._state['system_name'])
@@ -3123,6 +3120,8 @@ class StorwizeSVCCommonDriver(san.SanDriver,
                 location_info = ('StorwizeSVCDriver:%(sys_id)s:%(pool)s' %
                                  {'sys_id': self._state['system_id'],
                                   'pool': pool_data['name']})
+                multiattach = (self.configuration.
+                               storwize_svc_multihostmap_enabled)
                 pool_stats = {
                     'pool_name': pool_data['name'],
                     'total_capacity_gb': total_capacity_gb,
@@ -3135,7 +3134,8 @@ class StorwizeSVCCommonDriver(san.SanDriver,
                     'QoS_support': QoS_support,
                     'consistencygroup_support': True,
                     'location_info': location_info,
-                    'easytier_support': easy_tier
+                    'easytier_support': easy_tier,
+                    'multiattach': multiattach
                 }
             if self._replication_enabled:
                 pool_stats.update({
