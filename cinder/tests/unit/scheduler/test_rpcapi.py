30d22499be16ea89328b863366c9706ebab8ab2c
Michał Dulko <michal.dulko@intel.com>
2016-04-13 14:44:19 +0200

    Remove support for 1.x scheduler RPC API
    This commit gets rid of most of our Liberty compatibility code in
    scheduler RPC API.
    
    3 shims in scheduler.manager need to stay through Newton, as I've missed
    the opportunity to reorganize arguments in RPC methods when introducing
    SchedulerV1Proxy. It's a shame, but only thing I can do safely now is to
    update TODO comments there.
    
    Change-Id: I78a7bf78482e04868950ef02debb891bd139796c

--- cinder/tests/unit/scheduler/test_rpcapi.py
+++ cinder/tests/unit/scheduler/test_rpcapi.py
@@ -75,8 +75,7 @@ class SchedulerRpcAPITestCase(test.TestCase):
                 for kwarg, value in self.fake_kwargs.items():
                     self.assertEqual(expected_msg[kwarg], value)
 
-    @mock.patch('oslo_messaging.RPCClient.can_send_version', return_value=True)
-    def test_update_service_capabilities(self, can_send_version):
+    def test_update_service_capabilities(self):
         self._test_scheduler_api('update_service_capabilities',
                                  rpc_method='cast',
                                  service_name='fake_name',
@@ -84,23 +83,8 @@ class SchedulerRpcAPITestCase(test.TestCase):
                                  capabilities='fake_capabilities',
                                  fanout=True,
                                  version='2.0')
-        can_send_version.assert_called_once_with('2.0')
 
-    @mock.patch('oslo_messaging.RPCClient.can_send_version',
-                return_value=False)
-    def test_update_service_capabilities_old(self, can_send_version):
-        self._test_scheduler_api('update_service_capabilities',
-                                 rpc_method='cast',
-                                 service_name='fake_name',
-                                 host='fake_host',
-                                 capabilities='fake_capabilities',
-                                 fanout=True,
-                                 version='1.0')
-        can_send_version.assert_called_once_with('2.0')
-
-    @mock.patch('oslo_messaging.RPCClient.can_send_version',
-                return_value=True)
-    def test_create_volume(self, can_send_version):
+    def test_create_volume(self):
         self._test_scheduler_api('create_volume',
                                  rpc_method='cast',
                                  topic='topic',
@@ -111,26 +95,8 @@ class SchedulerRpcAPITestCase(test.TestCase):
                                  filter_properties='filter_properties',
                                  volume='volume',
                                  version='2.0')
-        can_send_version.assert_called_once_with('2.0')
 
-    @mock.patch('oslo_messaging.RPCClient.can_send_version',
-                return_value=False)
-    def test_create_volume_old(self, can_send_version):
-        # Tests backwards compatibility with older clients
-        self._test_scheduler_api('create_volume',
-                                 rpc_method='cast',
-                                 topic='topic',
-                                 volume_id='volume_id',
-                                 snapshot_id='snapshot_id',
-                                 image_id='image_id',
-                                 request_spec='fake_request_spec',
-                                 filter_properties='filter_properties',
-                                 version='1.2')
-        can_send_version.assert_has_calls([mock.call('2.0'), mock.call('1.9')])
-
-    @mock.patch('oslo_messaging.RPCClient.can_send_version',
-                return_value=True)
-    def test_migrate_volume_to_host(self, can_send_version):
+    def test_migrate_volume_to_host(self):
         self._test_scheduler_api('migrate_volume_to_host',
                                  rpc_method='cast',
                                  topic='topic',
@@ -141,27 +107,8 @@ class SchedulerRpcAPITestCase(test.TestCase):
                                  filter_properties='filter_properties',
                                  volume='volume',
                                  version='2.0')
-        can_send_version.assert_called_once_with('2.0')
 
-    @mock.patch('oslo_messaging.RPCClient.can_send_version',
-                return_value=False)
-    def test_migrate_volume_to_host_old(self, can_send_version):
-        self._test_scheduler_api('migrate_volume_to_host',
-                                 rpc_method='cast',
-                                 topic='topic',
-                                 volume_id='volume_id',
-                                 host='host',
-                                 force_host_copy=True,
-                                 request_spec='fake_request_spec',
-                                 filter_properties='filter_properties',
-                                 volume='volume',
-                                 version='1.3')
-        can_send_version.assert_has_calls([mock.call('2.0'),
-                                           mock.call('1.11')])
-
-    @mock.patch('oslo_messaging.RPCClient.can_send_version',
-                return_value=True)
-    def test_retype(self, can_send_version):
+    def test_retype(self):
         self._test_scheduler_api('retype',
                                  rpc_method='cast',
                                  topic='topic',
@@ -170,24 +117,8 @@ class SchedulerRpcAPITestCase(test.TestCase):
                                  filter_properties='filter_properties',
                                  volume='volume',
                                  version='2.0')
-        can_send_version.assert_called_with('2.0')
 
-    @mock.patch('oslo_messaging.RPCClient.can_send_version',
-                return_value=False)
-    def test_retype_old(self, can_send_version):
-        self._test_scheduler_api('retype',
-                                 rpc_method='cast',
-                                 topic='topic',
-                                 volume_id='volume_id',
-                                 request_spec='fake_request_spec',
-                                 filter_properties='filter_properties',
-                                 volume='volume',
-                                 version='1.4')
-        can_send_version.assert_has_calls([mock.call('2.0'),
-                                           mock.call('1.10')])
-
-    @mock.patch('oslo_messaging.RPCClient.can_send_version', return_value=True)
-    def test_manage_existing(self, can_send_version):
+    def test_manage_existing(self):
         self._test_scheduler_api('manage_existing',
                                  rpc_method='cast',
                                  topic='topic',
@@ -195,33 +126,9 @@ class SchedulerRpcAPITestCase(test.TestCase):
                                  request_spec='fake_request_spec',
                                  filter_properties='filter_properties',
                                  version='2.0')
-        can_send_version.assert_called_with('2.0')
 
-    @mock.patch('oslo_messaging.RPCClient.can_send_version',
-                return_value=False)
-    def test_manage_existing_old(self, can_send_version):
-        self._test_scheduler_api('manage_existing',
-                                 rpc_method='cast',
-                                 topic='topic',
-                                 volume_id='volume_id',
-                                 request_spec='fake_request_spec',
-                                 filter_properties='filter_properties',
-                                 version='1.5')
-        can_send_version.assert_called_with('2.0')
-
-    @mock.patch('oslo_messaging.RPCClient.can_send_version', return_value=True)
-    def test_get_pools(self, can_send_version):
+    def test_get_pools(self):
         self._test_scheduler_api('get_pools',
                                  rpc_method='call',
                                  filters=None,
                                  version='2.0')
-        can_send_version.assert_called_with('2.0')
-
-    @mock.patch('oslo_messaging.RPCClient.can_send_version',
-                return_value=False)
-    def test_get_pools_old(self, can_send_version):
-        self._test_scheduler_api('get_pools',
-                                 rpc_method='call',
-                                 filters=None,
-                                 version='1.7')
-        can_send_version.assert_called_with('2.0')
