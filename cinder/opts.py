78d124dee28e83a4718a455c456605b8127eab09
arsenc <arsenc@synology.com>
2016-06-23 16:06:01 +0800

    Add Synology DSM storage driver
    This driver supports below features:
    - Volume Create/Delete
    - Volume Attach/Detach
    - Snapshot Create/Delete
    - Create Volume from Snapshot
    - Get Volume Stats
    - Copy Image to Volume
    - Copy Volume to Image
    - Clone Volume
    - Extend Volume
    
    DocImpact
    Implements: blueprint synology-cinder-driver
    Co-Authored-By: Taylor Huang<taylorh@synology.com>
    
    Change-Id: I18d47f5d4cf10e4f481722406c6a5c071a521616

--- cinder/opts.py
+++ cinder/opts.py
@@ -152,6 +152,8 @@ from cinder.volume.drivers import scality as cinder_volume_drivers_scality
 from cinder.volume.drivers import sheepdog as cinder_volume_drivers_sheepdog
 from cinder.volume.drivers import smbfs as cinder_volume_drivers_smbfs
 from cinder.volume.drivers import solidfire as cinder_volume_drivers_solidfire
+from cinder.volume.drivers.synology import synology_common as \
+    cinder_volume_drivers_synology_synologycommon
 from cinder.volume.drivers import tegile as cinder_volume_drivers_tegile
 from cinder.volume.drivers import tintri as cinder_volume_drivers_tintri
 from cinder.volume.drivers.violin import v7000_common as \
@@ -306,6 +308,7 @@ def list_opts():
                 cinder_volume_drivers_hitachi_hbsdfc.volume_opts,
                 cinder_quota.quota_opts,
                 cinder_volume_drivers_huawei_huaweidriver.huawei_opts,
+                cinder_volume_drivers_synology_synologycommon.cinder_opts,
                 cinder_volume_drivers_dell_dellstoragecentercommon.
                 common_opts,
                 cinder_scheduler_hostmanager.host_manager_opts,
