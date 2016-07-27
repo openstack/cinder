4679d1fae04ef94e5126c3bca17d45ad6ef2deb9
Sean McGinnis <sean_mcginnis@dell.com>
2016-07-27 15:40:34 -0500

    Remove Tegile volume driver
    The Tegile CI has not reported on a patch since 2016-06-09 03:29:06.
    This is well past our policy for CI reporting.
    
    Per Cinder's non-compliance policy [1] this patch removes the driver
    and will be approved if the issue is not corrected within one week.
    
    [1] https://wiki.openstack.org/wiki/Cinder/tested-3rdParty-drivers#Non-Compliance_Policy
    
    Change-Id: If45461c4eff11e42ed65d2eb87573ab681c5ac93

--- cinder/exception.py
+++ cinder/exception.py
@@ -1189,11 +1189,6 @@ class CohoException(VolumeDriverException):
     message = _("Coho Data Cinder driver failure: %(message)s")
 
 
-# Tegile Storage drivers
-class TegileAPIException(VolumeBackendAPIException):
-    message = _("Unexpected response from Tegile IntelliFlash API")
-
-
 # NexentaStor driver exception
 class NexentaException(VolumeDriverException):
     message = _("%(message)s")
