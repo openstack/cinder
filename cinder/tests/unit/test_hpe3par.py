9f59a3a08bfe9c8e8d1363029470dd1247bca873
stack <vijay.ladani@hpe.com>
2016-08-18 23:45:44 -0700

    Handle snapshot delete conflict in 3par driver
    Issue is most likely due to the fact that 3par uses online
    copy during create from snapshot.   This process on the 3PAR
    creates temporary snapshots for doing the copy.  They get
    deleted after the online (background) copy is completed.
    If user try and delete the volume during that process, user
    will get an HTTP Conflict and the "volume has a child" error.
    
    Fix is to detect conflict code and remove temporary online
    copies if they exist and delete snapshot again.
    
    Modified unit test to check HTTP conflict with error code
    
    Closes-Bug: #1600104
    (cherry picked from commit 1e8204795e240303795d49d8268233220c9455eb)
    
    Change-Id: If4a2cabb7afd0299c78e42a941b95a01c76b2b5b

--- cinder/tests/unit/test_hpe3par.py
+++ cinder/tests/unit/test_hpe3par.py
@@ -2350,6 +2350,7 @@ class HPE3PARBaseDriver(object):
             self.driver.create_volume_from_snapshot(self.volume, self.snapshot)
 
             ex = hpeexceptions.HTTPConflict("In use")
+            ex._error_code = 32
             mock_client.deleteVolume = mock.Mock(side_effect=ex)
 
             # Deleting the snapshot that a volume is dependent on should fail
