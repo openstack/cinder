96948e48b140a9a47d26bf805230df41a44c9c00
Kaitlin Farr <kaitlin.farr@jhuapl.edu>
2016-09-07 13:21:33 -0400

    Modifies override logic for key_manager
    Makes the logic for overriding config options for the key_manager
    more robust.
    
    Before this patch, the override logic seemed to be called before the global
    CONF object has been populated with values from the configuration file.
    ConfKeyManager, the default for if no value had been specified, would be
    used to override the value for api_class. Then when CONF was populated
    with the actual values, the ConfKeyManager override value would still be
    set.
    
    This patch makes the logic a little bit more robust so that the value
    is only overriden if explicitly passed into the function, not at the
    global scope outside of the function.
    
    SecurityImpact
    
    Closes-Bug: 1621109
    Change-Id: Id5f83f69fd3a877459fab924c005047e55f98c7b
    (cherry picked from commit b66d4d997cf371d4d451aa9e57d351f4e045fc8d)

--- cinder/keymgr/__init__.py
+++ cinder/keymgr/__init__.py
@@ -25,52 +25,62 @@ LOG = logging.getLogger(__name__)
 
 CONF = cfg.CONF
 
-castellan_opts.set_defaults(cfg.CONF)
+castellan_opts.set_defaults(CONF)
 
 # NOTE(kfarr): This line can be removed when a value is assigned in DevStack
 CONF.set_default('api_class', 'cinder.keymgr.conf_key_mgr.ConfKeyManager',
                  group='key_manager')
 
-# NOTE(kfarr): For backwards compatibility, everything below this comment
-# is deprecated for removal
-api_class = None
-try:
-    api_class = CONF.key_manager.api_class
-except cfg.NoSuchOptError:
-    LOG.warning(_LW("key_manager.api_class is not set, will use deprecated "
-                    "option keymgr.api_class if set"))
-    try:
-        api_class = CONF.keymgr.api_class
-    except cfg.NoSuchOptError:
-        LOG.warning(_LW("keymgr.api_class is not set"))
 
-deprecated_barbican = 'cinder.keymgr.barbican.BarbicanKeyManager'
-barbican = 'castellan.key_manager.barbican_key_manager.BarbicanKeyManager'
-deprecated_mock = 'cinder.tests.unit.keymgr.mock_key_mgr.MockKeyManager'
-castellan_mock = ('castellan.tests.unit.key_manager.mock_key_manager.'
-                  'MockKeyManager')
-
-
-def log_deprecated_warning(deprecated, castellan):
-    versionutils.deprecation_warning(deprecated, versionutils.NEWTON,
+def log_deprecated_warning(deprecated_value, castellan):
+    versionutils.deprecation_warning(deprecated_value,
+                                     versionutils.deprecated.NEWTON,
                                      in_favor_of=castellan, logger=LOG)
 
-if api_class == deprecated_barbican:
-    log_deprecated_warning(deprecated_barbican, barbican)
-    api_class = barbican
-elif api_class == deprecated_mock:
-    log_deprecated_warning(deprecated_mock, castellan_mock)
-    api_class = castellan_mock
-elif api_class is None:
-    # TODO(kfarr): key_manager.api_class should be set in DevStack, and this
-    # block can be removed
-    LOG.warning(_LW("key manager not set, using insecure default %s"),
-                castellan_mock)
-    api_class = castellan_mock
 
-CONF.set_override('api_class', api_class, 'key_manager')
+# NOTE(kfarr): this method is for backwards compatibility, it is deprecated
+# for removal
+def set_overrides(conf):
+    api_class = None
+    should_override = False
+    try:
+        api_class = conf.key_manager.api_class
+    except cfg.NoSuchOptError:
+        LOG.warning(_LW("key_manager.api_class is not set, will use deprecated"
+                        " option keymgr.api_class if set"))
+        try:
+            api_class = CONF.keymgr.api_class
+            should_override = True
+        except cfg.NoSuchOptError:
+            LOG.warning(_LW("keymgr.api_class is not set"))
+
+    deprecated_barbican = 'cinder.keymgr.barbican.BarbicanKeyManager'
+    barbican = 'castellan.key_manager.barbican_key_manager.BarbicanKeyManager'
+    deprecated_mock = 'cinder.tests.unit.keymgr.mock_key_mgr.MockKeyManager'
+    castellan_mock = ('castellan.tests.unit.key_manager.mock_key_manager.'
+                      'MockKeyManager')
+
+    if api_class == deprecated_barbican:
+        should_override = True
+        log_deprecated_warning(deprecated_barbican, barbican)
+        api_class = barbican
+    elif api_class == deprecated_mock:
+        should_override = True
+        log_deprecated_warning(deprecated_mock, castellan_mock)
+        api_class = castellan_mock
+    elif api_class is None:
+        should_override = True
+        # TODO(kfarr): key_manager.api_class should be set in DevStack, and
+        # this block can be removed
+        LOG.warning(_LW("key manager not set, using insecure default %s"),
+                    castellan_mock)
+        api_class = castellan_mock
+
+    if should_override:
+        conf.set_override('api_class', api_class, 'key_manager')
 
 
 def API(conf=CONF):
+    set_overrides(conf)
     cls = importutils.import_class(conf.key_manager.api_class)
     return cls(conf)
