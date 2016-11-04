6be447f5d5459aba6647cb39f760f6ab164ea812
xianming mao <xianming.mao@easystack.cn>
2016-11-04 11:01:49 +0800

    cors: update default configuration
    Set_defaults has been added into oslo_middleware. so we use it
    to override the configuration defaults.
    More details: https://review.openstack.org/#/c/285368/
    Change-Id: I07a4438525bd941821269c0881dcfcd3355c6445

--- cinder/common/config.py
+++ cinder/common/config.py
@@ -201,28 +201,27 @@ CONF.register_opts(global_opts)
 
 def set_middleware_defaults():
     """Update default configuration options for oslo.middleware."""
-    # CORS Defaults
-    # TODO(krotscheck): Update with https://review.openstack.org/#/c/285368/
-    cfg.set_defaults(cors.CORS_OPTS,
-                     allow_headers=['X-Auth-Token',
-                                    'X-Identity-Status',
-                                    'X-Roles',
-                                    'X-Service-Catalog',
-                                    'X-User-Id',
-                                    'X-Tenant-Id',
-                                    'X-OpenStack-Request-ID',
-                                    'X-Trace-Info',
-                                    'X-Trace-HMAC',
-                                    'OpenStack-API-Version'],
-                     expose_headers=['X-Auth-Token',
-                                     'X-Subject-Token',
-                                     'X-Service-Token',
-                                     'X-OpenStack-Request-ID',
-                                     'OpenStack-API-Version'],
-                     allow_methods=['GET',
-                                    'PUT',
-                                    'POST',
-                                    'DELETE',
-                                    'PATCH',
-                                    'HEAD']
-                     )
+
+    cors.set_defaults(
+        allow_headers=['X-Auth-Token',
+                       'X-Identity-Status',
+                       'X-Roles',
+                       'X-Service-Catalog',
+                       'X-User-Id',
+                       'X-Tenant-Id',
+                       'X-OpenStack-Request-ID',
+                       'X-Trace-Info',
+                       'X-Trace-HMAC',
+                       'OpenStack-API-Version'],
+        expose_headers=['X-Auth-Token',
+                        'X-Subject-Token',
+                        'X-Service-Token',
+                        'X-OpenStack-Request-ID',
+                        'OpenStack-API-Version'],
+        allow_methods=['GET',
+                       'PUT',
+                       'POST',
+                       'DELETE',
+                       'PATCH',
+                       'HEAD']
+    )
