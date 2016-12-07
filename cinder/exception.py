5c815388e2d8d4b62f7a66dd14d07ce961143435
Kazumasa Nomura <kazumasa.nomura.rx@hitachi.com>
2016-12-07 20:16:16 +0900

    Introduce Hitachi VSP driver
    This patch introduces Hitachi VSP FC driver.
    
    Signed-off-by: "Kazumasa Nomura <kazumasa.nomura.rx@hitachi.com>"
    
    DocImpact
    Implements: blueprint hitachi-vsp-fc-driver
    
    Change-Id: Ife99bf138c86a2f9be91298064513073271f1239

--- cinder/exception.py
+++ cinder/exception.py
@@ -1097,6 +1097,19 @@ class HBSDVolumeIsBusy(VolumeIsBusy):
     message = _("Volume %(volume_name)s is busy.")
 
 
+# Hitachi VSP Driver
+class VSPError(VolumeDriverException):
+    message = _("VSP error occurred. %(message)s")
+
+
+class VSPBusy(VSPError):
+    message = _("Device or resource is busy.")
+
+
+class VSPNotSupported(VSPError):
+    message = _("The function on the storage is not supported.")
+
+
 # Datera driver
 class DateraAPIException(VolumeBackendAPIException):
     message = _("Bad response from Datera API")
