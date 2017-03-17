0a7ce3b2d77b4685cc293a39984805d12d3fd24a
luqitao <qtlu@fiberhome.com>
2017-03-17 01:59:13 -0400

    Use HostAddressOpt for opts that accept IP and hostnames
    Some configuration options were accepting both IP addresses
    and hostnames. Since there was no specific OSLO opt type to
    support this, we were using ``StrOpt``. The change [1] that
    added support for ``HostAddressOpt`` type was merged in Ocata
    and became available for use with oslo version 3.22.
    
    This patch changes the opt type of configuration options to use
    this more relevant opt type - HostAddressOpt.
    
    [1] I77bdb64b7e6e56ce761d76696bc4448a9bd325eb
    
    Change-Id: Ia5df1e10dbe76040b89b989b8914b2a9b77ad856

--- cinder/service.py
+++ cinder/service.py
@@ -67,9 +67,10 @@ service_opts = [
                help='Range, in seconds, to randomly delay when starting the'
                     ' periodic task scheduler to reduce stampeding.'
                     ' (Disable by setting to 0)'),
-    cfg.StrOpt('osapi_volume_listen',
-               default="0.0.0.0",
-               help='IP address on which OpenStack Volume API listens'),
+    cfg.HostAddressOpt('osapi_volume_listen',
+                       default="0.0.0.0",
+                       help='IP address on which OpenStack Volume API '
+                            'listens'),
     cfg.PortOpt('osapi_volume_listen_port',
                 default=8776,
                 help='Port on which OpenStack Volume API listens'),
