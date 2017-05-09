788ab6af1907340bf9e4fa60344c4ec6ffe0f2ca
wangxiyuan <wangxiyuan@huawei.com>
2017-05-09 18:32:00 +0800

    Fix backup_api_class doesn't work
    The backup_api_class should be called after the CONF initialised.
    
    Change-Id: Ifd31c855a1647faa7718d36b6fa13f7ae83a49eb
    Closes-bug: #1689523

--- cinder/backup/__init__.py
+++ cinder/backup/__init__.py
@@ -23,4 +23,7 @@ from cinder.common import config
 
 CONF = config.CONF
 
-API = importutils.import_class(CONF.backup_api_class)
+
+def API(*args, **kwargs):
+    class_name = CONF.backup_api_class
+    return importutils.import_object(class_name, *args, **kwargs)
