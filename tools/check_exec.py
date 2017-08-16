6460050c5c83a5218c91251f4f59a4449f0c30b3
Eric Harney <eharney@redhat.com>
2017-08-16 15:40:44 -0400

    Add releasenotes to check_exec list
    Prevent addition of executable files in
    the releasenotes/notes directory.
    
    Change-Id: I80ac0b7fce7b16a453fe9139f0dacf88325af8e7

--- tools/check_exec.py
+++ tools/check_exec.py
@@ -23,16 +23,17 @@ if len(sys.argv) < 2:
     print("Usage: %s <directory>" % sys.argv[0])
     sys.exit(1)
 
-directory = sys.argv[1]
+directories = sys.argv[1:]
 
 executable = []
 
-for root, mydir, myfile in os.walk(directory):
-    for f in myfile:
-        path = os.path.join(root, f)
-        mode = os.lstat(path).st_mode
-        if stat.S_IXUSR & mode:
-            executable.append(path)
+for d in directories:
+    for root, mydir, myfile in os.walk(d):
+        for f in myfile:
+            path = os.path.join(root, f)
+            mode = os.lstat(path).st_mode
+            if stat.S_IXUSR & mode:
+                executable.append(path)
 
 if executable:
     print("Executable files found:")
