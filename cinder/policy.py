43a315258129037905b32c39428406038084e604
TommyLike <tommylikehu@gmail.com>
2017-09-23 11:05:31 +0800

    [policy in code] Add support for attachment resource
    This is the basic patch which consits of the framework
    code for default policy in code feature as well as
    support in attachment resource.
    
    Change-Id: Ie3ff068e61ea8e0e8fff78deb732e183e036a10c
    Partial-Implements: blueprint policy-in-code

--- cinder/policy.py
+++ cinder/policy.py
@@ -15,23 +15,52 @@
 
 """Policy Engine For Cinder"""
 
+import sys
 
 from oslo_config import cfg
+from oslo_log import log as logging
 from oslo_policy import opts as policy_opts
 from oslo_policy import policy
+from oslo_utils import excutils
 
 from cinder import exception
+from cinder import policies
 
 CONF = cfg.CONF
+LOG = logging.getLogger(__name__)
 policy_opts.set_defaults(cfg.CONF, 'policy.json')
 
 _ENFORCER = None
 
 
-def init():
+def reset():
+    global _ENFORCER
+    if _ENFORCER:
+        _ENFORCER.clear()
+        _ENFORCER = None
+
+
+def init(policy_file=None, rules=None, default_rule=None, use_conf=True):
+    """Init an Enforcer class.
+
+        :param policy_file: Custom policy file to use, if none is specified,
+                          `CONF.policy_file` will be used.
+        :param rules: Default dictionary / Rules to use. It will be
+                    considered just in the first instantiation.
+        :param default_rule: Default rule to use, CONF.default_rule will
+                           be used if none is specified.
+        :param use_conf: Whether to load rules from config file.
+    """
+
     global _ENFORCER
     if not _ENFORCER:
-        _ENFORCER = policy.Enforcer(CONF)
+        _ENFORCER = policy.Enforcer(CONF,
+                                    policy_file=policy_file,
+                                    rules=rules,
+                                    default_rule=default_rule,
+                                    use_conf=use_conf)
+        register_rules(_ENFORCER)
+        _ENFORCER.load_rules()
 
 
 def enforce_action(context, action):
@@ -72,19 +101,100 @@ def enforce(context, action, target):
                              action=action)
 
 
-def check_is_admin(roles, context=None):
+def set_rules(rules, overwrite=True, use_conf=False):
+    """Set rules based on the provided dict of rules.
+
+       :param rules: New rules to use. It should be an instance of dict.
+       :param overwrite: Whether to overwrite current rules or update them
+                         with the new rules.
+       :param use_conf: Whether to reload rules from config file.
+    """
+
+    init(use_conf=False)
+    _ENFORCER.set_rules(rules, overwrite, use_conf)
+
+
+def get_rules():
+    if _ENFORCER:
+        return _ENFORCER.rules
+
+
+def register_rules(enforcer):
+    enforcer.register_defaults(policies.list_rules())
+
+
+def get_enforcer():
+    # This method is for use by oslopolicy CLI scripts. Those scripts need the
+    # 'output-file' and 'namespace' options, but having those in sys.argv means
+    # loading the Cinder config options will fail as those are not expected to
+    # be present. So we pass in an arg list with those stripped out.
+    conf_args = []
+    # Start at 1 because cfg.CONF expects the equivalent of sys.argv[1:]
+    i = 1
+    while i < len(sys.argv):
+        if sys.argv[i].strip('-') in ['namespace', 'output-file']:
+            i += 2
+            continue
+        conf_args.append(sys.argv[i])
+        i += 1
+
+    cfg.CONF(conf_args, project='cinder')
+    init()
+    return _ENFORCER
+
+
+def authorize(context, action, target, do_raise=True, exc=None):
+    """Verifies that the action is valid on the target in this context.
+
+       :param context: cinder context
+       :param action: string representing the action to be checked
+           this should be colon separated for clarity.
+           i.e. ``compute:create_instance``,
+           ``compute:attach_volume``,
+           ``volume:attach_volume``
+       :param target: dictionary representing the object of the action
+           for object creation this should be a dictionary representing the
+           location of the object e.g. ``{'project_id': context.project_id}``
+       :param do_raise: if True (the default), raises PolicyNotAuthorized;
+           if False, returns False
+       :param exc: Class of the exception to raise if the check fails.
+                   Any remaining arguments passed to :meth:`authorize` (both
+                   positional and keyword arguments) will be passed to
+                   the exception class. If not specified,
+                   :class:`PolicyNotAuthorized` will be used.
+
+       :raises cinder.exception.PolicyNotAuthorized: if verification fails
+           and do_raise is True. Or if 'exc' is specified it will raise an
+           exception of that type.
+
+       :return: returns a non-False value (not necessarily "True") if
+           authorized, and the exact value False if not authorized and
+           do_raise is False.
+    """
+    init()
+    credentials = context.to_policy_values()
+    if not exc:
+        exc = exception.PolicyNotAuthorized
+    try:
+        result = _ENFORCER.authorize(action, target, credentials,
+                                     do_raise=do_raise, exc=exc, action=action)
+    except policy.PolicyNotRegistered:
+        with excutils.save_and_reraise_exception():
+            LOG.exception('Policy not registered')
+    except Exception:
+        with excutils.save_and_reraise_exception():
+            LOG.error('Policy check for %(action)s failed with credentials '
+                      '%(credentials)s',
+                      {'action': action, 'credentials': credentials})
+    return result
+
+
+def check_is_admin(context):
     """Whether or not user is admin according to policy setting.
 
     """
     init()
-
-    # include project_id on target to avoid KeyError if context_is_admin
-    # policy definition is missing, and default admin_or_owner rule
-    # attempts to apply.
-    target = {'project_id': ''}
-    if context is None:
-        credentials = {'roles': roles}
-    else:
-        credentials = context.to_dict()
-
-    return _ENFORCER.enforce('context_is_admin', target, credentials)
+    # the target is user-self
+    credentials = context.to_policy_values()
+    target = credentials
+    return _ENFORCER.authorize('context_is_admin', target, credentials)
---
+++ cinder/tests/unit/test_policy.py
@@ -0,0 +1,131 @@
+# Copyright (c) 2017 Huawei Technologies Co., Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import os.path
+
+from oslo_config import cfg
+from oslo_config import fixture as config_fixture
+from oslo_policy import policy as oslo_policy
+
+from cinder import context
+from cinder import exception
+from cinder import test
+from cinder import utils
+
+from cinder import policy
+
+CONF = cfg.CONF
+
+
+class PolicyFileTestCase(test.TestCase):
+
+    def setUp(self):
+        super(PolicyFileTestCase, self).setUp()
+        self.context = context.get_admin_context()
+        self.target = {}
+        self.fixture = self.useFixture(config_fixture.Config(CONF))
+        self.addCleanup(policy.reset)
+
+    def test_modified_policy_reloads(self):
+        with utils.tempdir() as tmpdir:
+            tmpfilename = os.path.join(tmpdir, 'policy')
+            self.fixture.config(policy_file=tmpfilename, group='oslo_policy')
+            policy.reset()
+            policy.init()
+            rule = oslo_policy.RuleDefault('example:test', "")
+            policy._ENFORCER.register_defaults([rule])
+
+            action = "example:test"
+            with open(tmpfilename, "w") as policyfile:
+                policyfile.write('{"example:test": ""}')
+            policy.authorize(self.context, action, self.target)
+            with open(tmpfilename, "w") as policyfile:
+                policyfile.write('{"example:test": "!"}')
+            policy._ENFORCER.load_rules(True)
+            self.assertRaises(exception.PolicyNotAuthorized,
+                              policy.authorize,
+                              self.context, action, self.target)
+
+
+class PolicyTestCase(test.TestCase):
+
+    def setUp(self):
+        super(PolicyTestCase, self).setUp()
+        rules = [
+            oslo_policy.RuleDefault("true", '@'),
+            oslo_policy.RuleDefault("test:allowed", '@'),
+            oslo_policy.RuleDefault("test:denied", "!"),
+            oslo_policy.RuleDefault("test:my_file",
+                                    "role:compute_admin or "
+                                    "project_id:%(project_id)s"),
+            oslo_policy.RuleDefault("test:early_and_fail", "! and @"),
+            oslo_policy.RuleDefault("test:early_or_success", "@ or !"),
+            oslo_policy.RuleDefault("test:lowercase_admin",
+                                    "role:admin"),
+            oslo_policy.RuleDefault("test:uppercase_admin",
+                                    "role:ADMIN"),
+        ]
+        policy.reset()
+        policy.init()
+        # before a policy rule can be used, its default has to be registered.
+        policy._ENFORCER.register_defaults(rules)
+        self.context = context.RequestContext('fake', 'fake', roles=['member'])
+        self.target = {}
+        self.addCleanup(policy.reset)
+
+    def test_authorize_nonexistent_action_throws(self):
+        action = "test:noexist"
+        self.assertRaises(oslo_policy.PolicyNotRegistered, policy.authorize,
+                          self.context, action, self.target)
+
+    def test_authorize_bad_action_throws(self):
+        action = "test:denied"
+        self.assertRaises(exception.PolicyNotAuthorized, policy.authorize,
+                          self.context, action, self.target)
+
+    def test_authorize_bad_action_noraise(self):
+        action = "test:denied"
+        result = policy.authorize(self.context, action, self.target, False)
+        self.assertFalse(result)
+
+    def test_authorize_good_action(self):
+        action = "test:allowed"
+        result = policy.authorize(self.context, action, self.target)
+        self.assertTrue(result)
+
+    def test_templatized_authorization(self):
+        target_mine = {'project_id': 'fake'}
+        target_not_mine = {'project_id': 'another'}
+        action = "test:my_file"
+        policy.authorize(self.context, action, target_mine)
+        self.assertRaises(exception.PolicyNotAuthorized, policy.authorize,
+                          self.context, action, target_not_mine)
+
+    def test_early_AND_authorization(self):
+        action = "test:early_and_fail"
+        self.assertRaises(exception.PolicyNotAuthorized, policy.authorize,
+                          self.context, action, self.target)
+
+    def test_early_OR_authorization(self):
+        action = "test:early_or_success"
+        policy.authorize(self.context, action, self.target)
+
+    def test_ignore_case_role_check(self):
+        lowercase_action = "test:lowercase_admin"
+        uppercase_action = "test:uppercase_admin"
+        admin_context = context.RequestContext('admin',
+                                               'fake',
+                                               roles=['AdMiN'])
+        policy.authorize(admin_context, lowercase_action, self.target)
+        policy.authorize(admin_context, uppercase_action, self.target)
