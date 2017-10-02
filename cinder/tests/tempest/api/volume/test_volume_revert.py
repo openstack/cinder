395407c9578686ebc23f0bd4cbc567ac97bafbe9
Eric Harney <eharney@redhat.com>
2017-10-02 16:30:38 -0400

    Fix tempest test revet_client typo
    TrivialFix
    
    Change-Id: Ib4a0c50f699edb1cc2ea669ac711eb2f537be417

--- cinder/tests/tempest/api/volume/test_volume_revert.py
+++ cinder/tests/tempest/api/volume/test_volume_revert.py
@@ -31,7 +31,7 @@ class VolumeRevertTests(volume_base.BaseVolumeTest):
         super(VolumeRevertTests, cls).setup_clients()
 
         manager = cinder_clients.Manager(cls.os_primary)
-        cls.volume_revert_client = manager.volume_revet_client
+        cls.volume_revert_client = manager.volume_revert_client
 
     def setUp(self):
         super(VolumeRevertTests, self).setUp()
