adcfb4f97ecb33734ea6be8d043f8a50da294a0b
Chuck Short <chucks@redhat.com>
2018-08-28 10:01:51 -0400

    Simplify running pylint
    This commit does several things:
    
    - Setup and run pylint directly rather than running
      through a script. This allows the user to see what is happening
      while the user is running through pylint.
    - Allow the user to either run pylint on a particular changeset,
      or the entire cinder tree.
    - Allow the user to run on a particular changeset. Using like
      HEAD~1, etc.
    - Since the pylint gate check I disabled the tests that were
      reported by pylint. The thought here would be go through
      the failures and correct them.
    - Update pylint to 2.1.1.
    - Removed old pylintrc.
    
    Change-Id: I708c93843d991e8c0ac114b0b2d6ede373b96d0a
    Signed-off-by: Chuck Short <chucks@redhat.com>

---
+++ tools/coding-checks.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+set -eu
+
+usage() {
+    echo "Usage: $0 [OPTION]..."
+    echo "Run Cinder's coding check(s)"
+    echo ""
+    echo " -Y, --pylint [<basecommit>] Run pylint check on the entire cinder module or just files changed in basecommit (e.g. HEAD~1)"
+    echo " -h, --help   Print this usage message"
+    echo
+    exit 0
+}
+
+process_options() {
+    i=1
+    while [ $i -le $# ]; do
+        eval opt=\$$i
+        case $opt in
+            -h|--help) usage;;
+            -Y|--pylint) pylint=1;;
+            *) scriptargs="$scriptargs $opt"
+        esac
+        i=$((i+1))
+    done
+}
+
+run_pylint() {
+    local target="${scriptargs:-HEAD~1}"
+
+    if [[ "$target" = *"all"* ]]; then
+        files="cinder"
+    else
+        files=$(git diff --name-only --diff-filter=ACMRU $target "*.py")
+    fi
+
+    if [ -n "${files}" ]; then
+        echo "Running pylint against:"
+        printf "\t%s\n" "${files[@]}"
+        pylint --rcfile=.pylintrc --output-format=colorized ${files} -E \
+            -j `python -c 'import multiprocessing as mp; print(mp.cpu_count())'`
+    else
+        echo "No python changes in this commit, pylint check not required."
+        exit 0
+    fi
+}
+
+scriptargs=
+pylint=1
+
+process_options $@
+
+if [ $pylint -eq 1 ]; then
+    run_pylint
+    exit 0
+fi
