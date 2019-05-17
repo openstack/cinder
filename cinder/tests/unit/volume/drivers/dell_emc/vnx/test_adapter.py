053c33cbfc2b4b72d9ca20f617e542452f98944f
Yong Huang <yong.huang@emc.com>
2019-05-17 10:03:46 +0800

    [VNX] Fix test case issue
    Major changes:
    1. Fix mock file issue
    2. Fix cg test case issue
    
    Change-Id: Ia95936f3cea611f9fb7517d3b6849f511be2450b
    Closes-bug: #1795957
    (cherry picked from commit 18fbbd1afe2b9151fe521d200b280ae4a0ef6202)

--- cinder/tests/unit/volume/drivers/dell_emc/vnx/test_adapter.py
+++ cinder/tests/unit/volume/drivers/dell_emc/vnx/test_adapter.py
@@ -211,9 +211,9 @@ class TestCommonAdapter(test_base.TestCase):
             vnx_common.do_create_cg_from_cgsnap(
                 cg_id, cg_host, volumes, cgsnap_id, snaps))
         self.assertIsNone(model_update)
-        self.assertIsNotNone(
-            re.findall('id^12',
-                       volume_updates[0]['provider_location']))
+        provider_location = re.findall('id\^12',
+                                       volume_updates[0]['provider_location'])
+        self.assertEqual(1, len(provider_location))
 
     @res_mock.patch_common_adapter
     def test_create_cloned_cg(self, common, _):
@@ -270,9 +270,9 @@ class TestCommonAdapter(test_base.TestCase):
         model_update, volume_updates = vnx_common.do_clone_cg(
             cg_id, cg_host, volumes, src_cg_id, src_volumes)
         self.assertIsNone(model_update)
-        self.assertIsNotNone(
-            re.findall('id^12',
-                       volume_updates[0]['provider_location']))
+        provider_location = re.findall('id\^12',
+                                       volume_updates[0]['provider_location'])
+        self.assertEqual(1, len(provider_location))
 
     @res_mock.patch_common_adapter
     def test_parse_pools(self, vnx_common, mocked):
