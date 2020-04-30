9f0aae5b5876b6a85fc7a30a4acb1f33f76fdb03
Corey Bryant <corey.bryant@canonical.com>
2020-04-30 15:31:18 -0400

    Monkey patch original current_thread _active
    Monkey patch the original current_thread to use the up-to-date _active
    global variable. This solution is based on that documented at:
    https://github.com/eventlet/eventlet/issues/592
    
    Change-Id: Ida548b4bec00530418fd3d7ab254e971af77d3fe
    Closes-Bug: #1863021

--- cinder/cmd/backup.py
+++ cinder/cmd/backup.py
@@ -26,6 +26,13 @@ import sys
 # share the same context.
 import eventlet
 eventlet.monkey_patch()
+# Monkey patch the original current_thread to use the up-to-date _active
+# global variable. See https://bugs.launchpad.net/bugs/1863021 and
+# https://github.com/eventlet/eventlet/issues/592
+import __original_module_threading as orig_threading
+import threading # noqa
+orig_threading.current_thread.__globals__['_active'] = threading._active
+
 from oslo_concurrency import processutils
 from oslo_config import cfg
 from oslo_log import log as logging
