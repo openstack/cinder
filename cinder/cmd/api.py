cc9014ab4298a4509fd6139fe5b9ca4c7c69120d
Corey Bryant <corey.bryant@canonical.com>
2020-04-30 15:31:18 -0400

    Monkey patch original current_thread _active
    Monkey patch the original current_thread to use the up-to-date _active
    global variable. This solution is based on that documented at:
    https://github.com/eventlet/eventlet/issues/592
    
    Change-Id: Ida548b4bec00530418fd3d7ab254e971af77d3fe
    Closes-Bug: #1863021
    (cherry picked from commit 9f0aae5b5876b6a85fc7a30a4acb1f33f76fdb03)

--- cinder/cmd/api.py
+++ cinder/cmd/api.py
@@ -22,6 +22,13 @@ import sys
 
 import eventlet  # noqa
 eventlet.monkey_patch()
+# Monkey patch the original current_thread to use the up-to-date _active
+# global variable. See https://bugs.launchpad.net/bugs/1863021 and
+# https://github.com/eventlet/eventlet/issues/592
+import __original_module_threading as orig_threading
+import threading # noqa
+orig_threading.current_thread.__globals__['_active'] = threading._active
+
 from oslo_config import cfg
 from oslo_log import log as logging
 from oslo_reports import guru_meditation_report as gmr
