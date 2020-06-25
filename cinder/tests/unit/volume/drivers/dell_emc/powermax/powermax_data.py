3fadd92b64503f65c369841e924748bf7992059d
Helen Walsh <helen.walsh@emc.com>
2020-06-25 17:38:24 +0100

    PowerMax Ussuri Driver -  Setting the minimum version of U4P
    This is applicable to Ussuri only as we are moving to the 92
    in Victoria.  A replication refresh issue has been fixed in Unisphere
    for PowerMax 9.1.0.17 and has recently become generally available.
    
    Change-Id: Iae17c0987292c1c89b963b556630649afd26c4d8

--- cinder/tests/unit/volume/drivers/dell_emc/powermax/powermax_data.py
+++ cinder/tests/unit/volume/drivers/dell_emc/powermax/powermax_data.py
@@ -1274,7 +1274,7 @@ class PowerMaxData(object):
 
     data_dict = {volume_id: volume_info_dict}
     platform = 'Linux-4.4.0-104-generic-x86_64-with-Ubuntu-16.04-xenial'
-    unisphere_version = u'V9.1.0.14'
+    unisphere_version = u'V9.1.0.17'
     unisphere_version_90 = "V9.0.0.1"
     openstack_release = '12.0.0.0b3.dev401'
     openstack_version = '12.0.0'
