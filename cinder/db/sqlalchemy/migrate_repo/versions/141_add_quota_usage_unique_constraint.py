95fcb50a422fbd1949bd99b8cf1276a8de1f2556
Gorka Eguileor <geguileo@redhat.com>
2021-03-11 09:49:32 +0100

    Fix quota usage duplicate entries
    Our current quota system has a race condition on reservations that only
    happens when we are creating new entries in the quota_usages table.
    
    We normally lock quota_usages rows using a SELECT ... FOR UPDATE query,
    but that's only effective when the entries exist, and current code just
    creates them and proceeds without a lock on them.
    
    This, together with the table not having unique constraint means that we
    can get duplicated entries and we can have one entry overwriting the
    data written by another request.
    
    The quota_usages table does soft deletes, so the project_id and resource
    fields are not enough for a unique constraint, so we add a new column
    called race_preventer so we can have a unique constraint with the
    3 fields.
    
    Additionally we have to make sure that we acquire the locks before doing
    the reservation calculations or the syncs, so once we create any missing
    entries we close the session and try to get the locks again.
    
    With these 2 changes we'll avoid duplicated entries as well as avoid
    getting our quota usage out of sync right from the start.
    
    For the unique constraint part of the code there were 2 alternatives
    (one was even used in an earlier patchset):
    
    - Create a virtual/computed column for the Table that sets it to a fixed
      value when deleted_at is NULL and to NULL in any other case, then use
      this virtual/computed column together with project_id and resource
      fields for a unique constraint.
    
      This change was my preferred solution, but it requires bumping the
      SQLAlchemy version to 1.3.11 where the feature was added as computed
      columns [1] and in some DB engines requires a relatively new version,
      for example for PostgreSQL is only supported on version 12 or later.
    
    - Set deleted_at to a non NULL value by default on creation, and make
      sure our code always uses the deleted field to filter values.
    
      This is a bit nasty, but it has the advantage of not requiring new DB
      fields, no DB data migrations for existing entries, and easy to
      rollback once we figure out the underlying issue (although it may
      require a DB data migration on rollback if we want to leave the
      deleted_at entry at NULL).
    
    The decision to add a new field was because one of the alternatives is
    kind of hacky and the other one depends on specific DBMS versions and
    requires a SQLAlchemy version bump.
    
    [1]: https://docs.sqlalchemy.org/en/13/core/defaults.html#computed-generated-always-as-columns
    
    Closes-Bug: #1484343
    Change-Id: I9000c16c5b3e6f313f02256a10cb4bc0a26379f7
    (cherry picked from commit 1fb0767d8874f993e4ac6d9fda92933f1c5b6d3f)

---
+++ cinder/db/sqlalchemy/migrate_repo/versions/141_add_quota_usage_unique_constraint.py
@@ -0,0 +1,37 @@
+# Copyright 2021 Red Hat, Inc.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from sqlalchemy import Boolean, Column, MetaData, Table
+from migrate.changeset import constraint
+
+
+def upgrade(migrate_engine):
+    """Update quota_usages table to prevent races on creation.
+
+    Add race_preventer field and a unique constraint to prevent quota usage
+    duplicates and races that mess the quota system when first creating rows.
+    """
+    # There's no need to set the race_preventer field for existing DB entries,
+    # since the race we want to prevent is only on creation.
+    meta = MetaData(bind=migrate_engine)
+    quota_usages = Table('quota_usages', meta, autoload=True)
+
+    if not hasattr(quota_usages.c, 'race_preventer'):
+        quota_usages.create_column(Column('race_preventer', Boolean,
+                                          nullable=True))
+    unique = constraint.UniqueConstraint(
+        'project_id', 'resource', 'race_preventer',
+        table=quota_usages)
+    unique.create(engine=migrate_engine)
