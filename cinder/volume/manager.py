2bd22f240e8fc4a66f34e4dffaf56d41e2563590
Hemna <waboring@hemna.com>
2021-07-21 10:56:29 -0400

    Fix extra_capabilities
    This patch ensures that when pools are being reported that
    the volume driver's configured extra_capabilities are inserted
    into each pool reported by the driver.  Without this the
    CapabilitiesFilter never sees the extra_capabilities.
    This is also backwards compatible where drivers don't report pools,
    the extra_capabilities are inserted correctly.
    
    Closes-Bug: #1937086
    Change-Id: I5e8fe131d06be22b77b1e083a51006daf9d0ed66

--- cinder/volume/manager.py
+++ cinder/volume/manager.py
@@ -2655,7 +2655,11 @@ class VolumeManager(manager.CleanableManager,
         volume_stats = get_stats()
 
         if self.extra_capabilities:
-            volume_stats.update(self.extra_capabilities)
+            if "pools" in volume_stats:
+                for pool in volume_stats["pools"]:
+                    pool.update(self.extra_capabilities)
+            else:
+                volume_stats.update(self.extra_capabilities)
         if volume_stats:
 
             # NOTE(xyang): If driver reports replication_status to be
