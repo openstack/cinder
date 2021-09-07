1bb851660e02aa9b3562aed172659f26abf4e041
Alan Bishop <abishop@redhat.com>
2021-09-07 08:28:02 -0700

    Update project personas for default_types
    The default_types policies previously had initial support for system
    and project scoped personas. However, cinder won't support scopes in
    Xena. The scoping aspects of the default_types policies are removed
    until support for scopes is added in Yoga.
    
    Some functional and unit tests are deleted in favor of new tests that
    use the secure-rbac test framework.
    
    Co-Authored-by: Rajat Dhasmana <rajatdhasmana@gmail.com>
    
    Change-Id: I9d0a6994d92033539d39a1ce2403c12b69511712

--- cinder/tests/unit/policies/test_default_volume_types.py
+++ cinder/tests/unit/policies/test_default_volume_types.py
@@ -17,10 +17,30 @@ from http import HTTPStatus
 from unittest import mock
 import uuid
 
+import ddt
+from webob import exc
+
+from cinder.api import api_utils
 from cinder.api import microversions as mv
+from cinder.api.v3 import default_types
 from cinder import db
+from cinder.policies import default_types as default_type_policies
+from cinder.tests.unit.api import fakes as fake_api
 from cinder.tests.unit import fake_constants
+from cinder.tests.unit.policies import base
 from cinder.tests.unit.policies import test_base
+from cinder.tests.unit import utils as test_utils
+
+
+class FakeProject(object):
+    def __init__(self, id=None, name=None):
+        if id:
+            self.id = id
+        else:
+            self.id = uuid.uuid4().hex
+        self.name = name
+        self.description = 'fake project description'
+        self.domain_id = 'default'
 
 
 class DefaultVolumeTypesPolicyTests(test_base.CinderPolicyTests):
@@ -32,7 +52,7 @@ class DefaultVolumeTypesPolicyTests(test_base.CinderPolicyTests):
     def setUp(self):
         super(DefaultVolumeTypesPolicyTests, self).setUp()
         self.volume_type = self._create_fake_type(self.admin_context)
-        self.project = self.FakeProject()
+        self.project = FakeProject()
         # Need to mock out Keystone so the functional tests don't require other
         # services
         _keystone_client = mock.MagicMock()
@@ -46,13 +66,6 @@ class DefaultVolumeTypesPolicyTests(test_base.CinderPolicyTests):
 
     def _get_project(self, project_id, *args, **kwargs):
         return self.project
-
-    class FakeProject(object):
-        def __init__(self, name=None):
-            self.id = uuid.uuid4().hex
-            self.name = name
-            self.description = 'fake project description'
-            self.domain_id = 'default'
 
     def test_system_admin_can_set_default(self):
         system_admin_context = self.system_admin_context
@@ -83,21 +96,6 @@ class DefaultVolumeTypesPolicyTests(test_base.CinderPolicyTests):
                                               mv.DEFAULT_TYPE_OVERRIDES)
 
         self.assertEqual(HTTPStatus.OK, response.status_int)
-
-    def test_project_admin_cannot_set_default_for_other_project(self):
-        admin_context = self.admin_context
-
-        path = '/v3/default-types/%s' % admin_context.project_id
-        body = {
-            'default_type':
-                {"volume_type": self.volume_type.id}
-        }
-        response = self._get_request_response(self.other_admin_context,
-                                              path, 'PUT', body=body,
-                                              microversion=
-                                              mv.DEFAULT_TYPE_OVERRIDES)
-
-        self.assertEqual(HTTPStatus.FORBIDDEN, response.status_int)
 
     @mock.patch.object(db, 'project_default_volume_type_get',
                        return_value=FakeDefaultType())
@@ -133,17 +131,6 @@ class DefaultVolumeTypesPolicyTests(test_base.CinderPolicyTests):
 
         self.assertEqual(HTTPStatus.OK, response.status_int)
 
-    def test_project_admin_cannot_get_default_for_other_project(self):
-        admin_context = self.admin_context
-
-        path = '/v3/default-types/%s' % admin_context.project_id
-        response = self._get_request_response(self.other_admin_context,
-                                              path, 'GET',
-                                              microversion=
-                                              mv.DEFAULT_TYPE_OVERRIDES)
-
-        self.assertEqual(HTTPStatus.FORBIDDEN, response.status_int)
-
     def test_system_admin_can_get_all_default(self):
         system_admin_context = self.system_admin_context
 
@@ -154,17 +141,6 @@ class DefaultVolumeTypesPolicyTests(test_base.CinderPolicyTests):
                                               mv.DEFAULT_TYPE_OVERRIDES)
 
         self.assertEqual(HTTPStatus.OK, response.status_int)
-
-    def test_project_admin_cannot_get_all_default(self):
-        admin_context = self.admin_context
-
-        path = '/v3/default-types'
-        response = self._get_request_response(admin_context,
-                                              path, 'GET',
-                                              microversion=
-                                              mv.DEFAULT_TYPE_OVERRIDES)
-
-        self.assertEqual(HTTPStatus.FORBIDDEN, response.status_int)
 
     def test_system_admin_can_unset_default(self):
         system_admin_context = self.system_admin_context
@@ -188,13 +164,130 @@ class DefaultVolumeTypesPolicyTests(test_base.CinderPolicyTests):
 
         self.assertEqual(HTTPStatus.NO_CONTENT, response.status_int)
 
-    def test_project_admin_cannot_unset_default_for_other_project(self):
-        admin_context = self.admin_context
 
-        path = '/v3/default-types/%s' % admin_context.project_id
-        response = self._get_request_response(self.other_admin_context,
-                                              path, 'DELETE',
-                                              microversion=
-                                              mv.DEFAULT_TYPE_OVERRIDES)
+@ddt.ddt
+class DefaultVolumeTypesPolicyTest(base.BasePolicyTest):
 
-        self.assertEqual(HTTPStatus.FORBIDDEN, response.status_int)
+    authorized_admins = [
+        'system_admin',
+        'legacy_admin',
+        'project_admin',
+    ]
+    unauthorized_admins = [
+        'legacy_owner',
+        'system_member',
+        'system_reader',
+        'system_foo',
+        'project_member',
+        'project_reader',
+        'project_foo',
+        'other_project_member',
+        'other_project_reader',
+    ]
+
+    # Basic policy test is without enforcing scope (which cinder doesn't
+    # yet support) and deprecated rules enabled.
+    def setUp(self, enforce_scope=False, enforce_new_defaults=False,
+              *args, **kwargs):
+        super().setUp(enforce_scope, enforce_new_defaults, *args, **kwargs)
+        self.controller = default_types.DefaultTypesController()
+        self.api_path = '/v3/default-types/%s' % (self.project_id)
+        self.api_version = mv.DEFAULT_TYPE_OVERRIDES
+
+    def _create_volume_type(self):
+        vol_type = test_utils.create_volume_type(self.project_admin_context,
+                                                 name='fake_vol_type',
+                                                 testcase_instance=self)
+        return vol_type
+
+    @ddt.data(*base.all_users)
+    @mock.patch.object(api_utils, 'get_project')
+    def test_default_type_set_policy(self, user_id, fake_project):
+        vol_type = self._create_volume_type()
+        fake_project.return_value = FakeProject(id=self.project_id)
+        rule_name = default_type_policies.CREATE_UPDATE_POLICY
+        url = self.api_path
+        req = fake_api.HTTPRequest.blank(url, version=self.api_version)
+        req.method = 'POST'
+        body = {"default_type": {"volume_type": vol_type.id}}
+        unauthorized_exceptions = [exc.HTTPForbidden]
+        self.common_policy_check(user_id, self.authorized_admins,
+                                 self.unauthorized_admins,
+                                 unauthorized_exceptions,
+                                 rule_name, self.controller.create_update, req,
+                                 id=vol_type.id, body=body)
+
+    @ddt.data(*base.all_users)
+    @mock.patch.object(default_types.db, 'project_default_volume_type_get')
+    @mock.patch.object(api_utils, 'get_project')
+    def test_default_type_get_policy(self, user_id, fake_project,
+                                     mock_default_get):
+        fake_project.return_value = FakeProject(id=self.project_id)
+        rule_name = default_type_policies.GET_POLICY
+        url = self.api_path
+        req = fake_api.HTTPRequest.blank(url, version=self.api_version)
+        unauthorized_exceptions = [exc.HTTPForbidden]
+        self.common_policy_check(user_id, self.authorized_admins,
+                                 self.unauthorized_admins,
+                                 unauthorized_exceptions,
+                                 rule_name, self.controller.detail, req,
+                                 id=self.project_id)
+
+    @ddt.data(*base.all_users)
+    @mock.patch.object(default_types.db, 'project_default_volume_type_get')
+    def test_default_type_get_all_policy(self, user_id, mock_default_get):
+        rule_name = default_type_policies.GET_ALL_POLICY
+        url = self.api_path
+        req = fake_api.HTTPRequest.blank(url, version=self.api_version)
+        unauthorized_exceptions = [exc.HTTPForbidden]
+        # NOTE: The users 'legacy_admin' and 'project_admin' pass for
+        # GET_ALL_POLICY since with enforce_new_defaults=False, we have
+        # a logical OR between old policy and new one hence RULE_ADMIN_API
+        # allows them to pass
+        self.common_policy_check(user_id, self.authorized_admins,
+                                 self.unauthorized_admins,
+                                 unauthorized_exceptions,
+                                 rule_name, self.controller.index, req)
+
+    @ddt.data(*base.all_users)
+    @mock.patch.object(api_utils, 'get_project')
+    @mock.patch.object(default_types.db, 'project_default_volume_type_get')
+    def test_default_type_unset_policy(self, user_id, mock_default_unset,
+                                       fake_project):
+        fake_project.return_value = FakeProject(id=self.project_id)
+        rule_name = default_type_policies.DELETE_POLICY
+        url = self.api_path
+        req = fake_api.HTTPRequest.blank(url, version=self.api_version)
+        req.method = 'DELETE'
+        unauthorized_exceptions = [exc.HTTPForbidden]
+        self.common_policy_check(user_id, self.authorized_admins,
+                                 self.unauthorized_admins,
+                                 unauthorized_exceptions,
+                                 rule_name, self.controller.delete, req,
+                                 id=self.project_id)
+
+
+class DefaultVolumeTypesPolicySecureRbacTest(DefaultVolumeTypesPolicyTest):
+
+    authorized_admins = [
+        'legacy_admin',
+        'system_admin',
+        'project_admin',
+    ]
+    unauthorized_admins = [
+        'legacy_owner',
+        'system_member',
+        'system_reader',
+        'system_foo',
+        'project_member',
+        'project_reader',
+        'project_foo',
+        'other_project_member',
+        'other_project_reader',
+    ]
+
+    def setUp(self, *args, **kwargs):
+        # Test secure RBAC by disabling deprecated policy rules (scope
+        # is still not enabled).
+        super().setUp(enforce_scope=False, enforce_new_defaults=True,
+                      *args, **kwargs)
