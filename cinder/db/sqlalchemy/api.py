86d3ffec4c44b86639c77850d1084dc3065294b8
whoami-rajat <rajatdhasmana@gmail.com>
2021-12-28 12:44:42 +0000

    Add cinder-manage command to update service_uuid
    In some deployments, after an upgrade, we remove the old service
    records and create new ones. This leaves the volumes with the
    service_uuid pointing to the old (deleted) service and causes an
    issue while purging out the deleted service records.
    This patch adds a cinder-manage command to update the
    service_uuid field of volumes with the following command:
    
    ``cinder-manage volume update_service``
    
    The service_uuid of volumes associated with old service_uuid
    also gets updated when cinder creates a new service.
    
    Change-Id: I0b13c6351733b8163bcf6e73c939c375493dcba5
    (cherry picked from commit edeac132a19dc05a4108c630ebdfd02de9fd92ef)
    (cherry picked from commit 530376bc5d9bb7a4a6cd70a723f3b3d4a7d309c4)
    (cherry picked from commit 5b3717f8bfa69c142778ffeabfc4ab91f1f23581)

--- cinder/db/sqlalchemy/api.py
+++ cinder/db/sqlalchemy/api.py
@@ -2862,6 +2862,32 @@ def volume_get_all_by_group(context, group_id, filters=None):
     return query.all()
 
 
+@require_admin_context
+@main_context_manager.writer
+def volume_update_all_by_service(context):
+    """Ensure volumes have the correct service_uuid value for their host.
+
+    In some deployment tools, when performing an upgrade, all service records
+    are recreated including c-vol service which gets a new record in the
+    services table, though its host name is constant. Later we then delete the
+    old service record.
+    As a consequence, the volumes have the right host name but the service
+    UUID needs to be updated to the ID of the new service record.
+
+    :param context: context to query under
+    """
+    # Get all cinder-volume services
+    services = service_get_all(context, binary='cinder-volume')
+    for service in services:
+        query = model_query(context, models.Volume)
+        query = query.filter(
+            _filter_host(
+                models.Volume.host, service.host),
+            models.Volume.service_uuid != service.uuid)
+        query.update(
+            {"service_uuid": service.uuid}, synchronize_session=False)
+
+
 @require_context
 @main_context_manager.reader
 def volume_get_all_by_generic_group(context, group_id, filters=None):
