43ca0d6e3c1cf3d98ef8d4f32bfc401bea3d01a6
Brian Rosmaita <rosmaita.fossdev@gmail.com>
2022-01-12 12:39:31 -0500

    Remove block-box files
    The block-box files were marked as "not supported" by change
    I36c23cc0f61d in January 2020.  Now it's time to remove the
    unsupported code.
    
    Change-Id: Iaa3a7892b9b3d9435de63afca7dce18d13c61685

--- contrib/block-box/docker-entrypoint-initdb.d/script.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE cinder;
-GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'localhost' \
-  IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON cinder.* TO 'cinder'@'%' \
-IDENTIFIED BY 'password';
--- contrib/block-box/scripts/cinder-api.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-INIT_DB=${INIT_DB:-true}
-
-if [ "$INIT_DB" = "true" ]; then
-/bin/sh -c "cinder-manage db sync"
-fi
-cinder-api -d
