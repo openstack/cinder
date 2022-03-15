90bf3633cc76003d931d008b48d437447a3f4a5f
Hemna <waboring@hemna.com>
2022-03-15 08:45:14 -0400

    [SAP] Replace CapacityFilter
    This patch reworks the CapacityFilter which uses the new
    utils.calculate_capacity_factors to determine if the
    virtual free space available.
    
    The main issue is that the vmware driver does lazy creates.
    This causes an issue of over reporting of free space by
    the backend, because the space hasn't been consumed yet.
    So the amount of free space is not accurate with respect to
    how much has been allocated by cinder.  This updated calculate
    capacity factors as well as the SAPCapacityFilter accounts for
    the virtual free space by using the cinder allocated capacity
    tracking.  If the free space is reported less than what cinder
    thinks should be available, then the reported free space is used.
    
    This relies on an acurate reporting of the allocated capacity
    by the driver.  We know there is an issue with allocated capacity
    not being reported correctly for migrated volumes, as well as
    accounting for existing volumes at startup.  The startup issue
    should be solved with this PR:
    https://github.com/sapcc/cinder/pull/117
    
    Will have to do a folllow up to account for updating the allocated
    capacity for migrated volumes.

--- cinder/scheduler/filters/capacity_filter.py
+++ cinder/scheduler/filters/capacity_filter.py
@@ -140,7 +140,7 @@ class CapacityFilter(filters.BaseBackendFilter):
                     "grouping": grouping,
                     "requested": requested_size,
                     "available": virtual_free_space,
-                    "provisioning_type": factors["provisioning_type"]}
+                    "provisioning_type": factors["provisioned_type"]}
         # Only evaluate using max_over_subscription_ratio if
         # thin_provisioning_support is True. Check if the ratio of
         # provisioned capacity over total capacity has exceeded over
@@ -181,9 +181,9 @@ class CapacityFilter(filters.BaseBackendFilter):
                 if not res:
                     LOG.warning("Insufficient free virtual space "
                                 "(%(available)sGB) to accommodate thin "
-                                "provisioned %(requested)sGB volume on "
-                                "%(grouping)s %(grouping_name)s."
-                                " %(provisioning_type)s).",
+                                "provisioned %(requested)sGB volume on"
+                                " %(grouping)s %(grouping_name)s"
+                                " %(provisioning_type)s.",
                                 msg_args)
                 else:
                     LOG.debug("Space information for volume creation "
@@ -210,7 +210,7 @@ class CapacityFilter(filters.BaseBackendFilter):
                         "%(provisioning_type)s", msg_args)
             return False
 
-        LOG.debug("Space information for volume creation "
+        LOG.debug("[SAP] Space information for volume creation "
                   "on %(grouping)s %(grouping_name)s (requested / avail): "
                   "%(requested)s/%(available)s %(provisioning_type)s.",
                   msg_args)
