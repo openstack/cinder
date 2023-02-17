e9dccb93be059176fec19b9a021672cbcc2f8414
Stephen Finucane <stephenfin@redhat.com>
2023-02-17 12:34:56 +0000

    db: Remove legacy migrations
    sqlalchemy-migrate does not (and will not) support sqlalchemy 2.0. We
    need to drop these migrations to ensure we can upgrade our sqlalchemy
    version.
    
    Change-Id: I39448af0eb8f4c557d591057760c27b1d40d3593
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- cinder/db/migration.py
+++ cinder/db/migration.py
@@ -21,9 +21,6 @@ import os
 from alembic import command as alembic_api
 from alembic import config as alembic_config
 from alembic import migration as alembic_migration
-from migrate import exceptions as migrate_exceptions
-from migrate.versioning import api as migrate_api
-from migrate.versioning import repository as migrate_repo
 from oslo_config import cfg
 from oslo_db import options
 from oslo_log import log as logging
@@ -33,20 +30,6 @@ from cinder.db.sqlalchemy import api as db_api
 options.set_defaults(cfg.CONF)
 
 LOG = logging.getLogger(__name__)
-
-MIGRATE_INIT_VERSION = 134
-ALEMBIC_INIT_VERSION = '921e1a36b076'
-
-
-def _find_migrate_repo():
-    """Get the project's change script repository
-
-    :returns: An instance of ``migrate.versioning.repository.Repository``
-    """
-    path = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), 'legacy_migrations',
-    )
-    return migrate_repo.Repository(path)
 
 
 def _find_alembic_conf():
@@ -66,35 +49,6 @@ def _find_alembic_conf():
     return config
 
 
-def _is_database_under_migrate_control(engine, repository):
-    try:
-        migrate_api.db_version(engine, repository)
-        return True
-    except migrate_exceptions.DatabaseNotControlledError:
-        return False
-
-
-def _is_database_under_alembic_control(engine):
-    with engine.connect() as conn:
-        context = alembic_migration.MigrationContext.configure(conn)
-        return bool(context.get_current_revision())
-
-
-def _init_alembic_on_legacy_database(engine, repository, config):
-    """Init alembic in an existing environment with sqlalchemy-migrate."""
-    LOG.info(
-        'The database is still under sqlalchemy-migrate control; '
-        'applying any remaining sqlalchemy-migrate-based migrations '
-        'and fake applying the initial alembic migration'
-    )
-    migrate_api.upgrade(engine, repository)
-
-    # re-use the connection rather than creating a new one
-    with engine.begin() as connection:
-        config.attributes['connection'] = connection
-        alembic_api.stamp(config, ALEMBIC_INIT_VERSION)
-
-
 def _upgrade_alembic(engine, config, version):
     # re-use the connection rather than creating a new one
     with engine.begin() as connection:
@@ -104,20 +58,11 @@ def _upgrade_alembic(engine, config, version):
 
 def db_version():
     """Get database version."""
-    repository = _find_migrate_repo()
     engine = db_api.get_engine()
 
-    migrate_version = None
-    if _is_database_under_migrate_control(engine, repository):
-        migrate_version = migrate_api.db_version(engine, repository)
-
-    alembic_version = None
-    if _is_database_under_alembic_control(engine):
-        with engine.connect() as conn:
-            m_context = alembic_migration.MigrationContext.configure(conn)
-            alembic_version = m_context.get_current_revision()
-
-    return alembic_version or migrate_version
+    with engine.connect() as conn:
+        m_context = alembic_migration.MigrationContext.configure(conn)
+        return m_context.get_current_revision()
 
 
 def db_sync(version=None, engine=None):
@@ -140,7 +85,6 @@ def db_sync(version=None, engine=None):
     if engine is None:
         engine = db_api.get_engine()
 
-    repository = _find_migrate_repo()
     config = _find_alembic_conf()
 
     # discard the URL encoded in alembic.ini in favour of the URL configured
@@ -155,17 +99,6 @@ def db_sync(version=None, engine=None):
     engine_url = str(engine.url).replace('%', '%%')
     config.set_main_option('sqlalchemy.url', str(engine_url))
 
-    # if we're in a deployment where sqlalchemy-migrate is already present,
-    # then apply all the updates for that and fake apply the initial alembic
-    # migration; if we're not then 'upgrade' will take care of everything
-    # this should be a one-time operation
-    if (
-        _is_database_under_migrate_control(engine, repository) and
-        not _is_database_under_alembic_control(engine)
-    ):
-        _init_alembic_on_legacy_database(engine, repository, config)
-
-    # apply anything later
     LOG.info('Applying migration(s)')
     _upgrade_alembic(engine, config, version)
     LOG.info('Migration(s) applied')
