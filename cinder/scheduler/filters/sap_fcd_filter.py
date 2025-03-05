ebee0f7f71b1cf59b1ccab374b83f7a75620b8dc
Walter Boring IV <waboring@hemna.com>
2025-03-05 13:14:29 -0500

    [SAP] Fix SAPFCDFilter
    This fixes an issue with the filter where the destination pool
    was not passed in to the migration request.
    
    This filter ensures a migration is allowed if:
    1) the pool is different on the same vcenter.
    2) if the vcenter is different the migration is only allowed on the same
       pool that the volume currently lives on. This prevents data movement.
    
    [SAP] update SAPFCDFilter add tests
    
    This patch updates the logic in the SAPFCDFilter to be more
    permissive.  Also added unit tests to test the scenarios
    of the filtering.
    
    Change-Id: Ie2a8fc502e335963246aeec64cb5b96ba9c169d3

--- cinder/scheduler/filters/sap_fcd_filter.py
+++ cinder/scheduler/filters/sap_fcd_filter.py
@@ -17,6 +17,7 @@
 from oslo_log import log as logging
 
 from cinder.scheduler import filters
+from cinder.volume.volume_utils import extract_host
 
 
 LOG = logging.getLogger(__name__)
@@ -38,35 +39,62 @@ class SAPFCDFilter(filters.BaseBackendFilter):
     def backend_passes(self, backend_state, filter_properties):
 
         if not self._is_vmware_fcd(backend_state):
-            LOG.info("Backend is not a VMware FCD backend")
             return True
 
         spec = filter_properties.get('request_spec', {})
         vol = spec.get('volume_properties', {})
 
         if spec.get('operation') != 'migrate_volume':
-            LOG.info("Operation is not a migrate_volume")
             return True
 
         # We are migrating a volume.  If we are migrating to a different
         # backend, we want to ensure that the pool is the same as the
         # original backend.
 
-        #   name@backend#pool
-        orig_host = vol.get('host')
-        orig_backend = orig_host.split('#')[0]
-        orig_pool = orig_host.split('#')[1]
-        destination_host = spec.get('destination_host')
+        # host@backend#pool
 
-        dest_backend = destination_host.split('#')[0]
-        dest_pool = destination_host.split('#')[1]
+        # This is the backend passed in to the filter.
+        filter_pool = extract_host(backend_state.host, 'pool')
+        filter_host = extract_host(backend_state.host, 'host')
+
+        # This is the original host, backend and pool that the volume
+        # was created on.
+        orig_host = vol.get('host')
+        orig_host_name = extract_host(orig_host, 'host')
+        # This returns name@backend.  We only want the backend.
+        orig_backend = extract_host(orig_host, 'backend').split('@')[1]
+        orig_pool = extract_host(orig_host, 'pool')
+
+        # This is the destination host, backend and pool that the volume
+        # is being migrated to.
+        # If the destination host provides a pool, we will ignore that
+        # pool, because we want it to move to the same pool on the
+        # new backend host first.  This prevents data movement.
+        # You can issue a migrate command with a destination pool
+        # if it's on the same host.
+        destination_host = spec.get('destination_host')
+        dest_backend = extract_host(destination_host, 'backend').split('@')[1]
 
         if orig_backend != dest_backend:
-            LOG.info("Destination backend is different from original backend")
-            # We only want to pass if the pool is the same as the original pool
-            if dest_pool == orig_pool:
-                return True
-            else:
-                return False
-        else:
+            LOG.debug("Allow migration to different backend %s %s %s",
+                      orig_backend, dest_backend, backend_state.host)
             return True
+
+        # we stay on the backend
+
+        # if we move to the same pool on _any_ other host, that's fine with us
+        if orig_pool == filter_pool:
+            LOG.debug("Allow migration to same pool %s %s %s",
+                      orig_pool, filter_pool, backend_state.host)
+            return True
+
+        # we switch pools
+
+        # if we move on the same host, it's fine if we switch pools
+        if orig_host_name == filter_host:
+            LOG.debug("Allow migration to same host %s %s %s",
+                      orig_host_name, filter_host, backend_state.host)
+            return True
+
+        LOG.debug("Deny migration to %s", backend_state.host)
+        return False
